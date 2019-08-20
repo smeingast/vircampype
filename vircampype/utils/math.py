ad1342ac0cc9ac7abe5a04acf63157cc209b8dbe
Stefan Meingast <stefan.meingast@gmail.com>
2019-08-20 13:20:13 +0200

    Added background estimator

---
+++ vircampype/utils/math.py
@@ -0,0 +1,100 @@
+# =========================================================================== #
+# Import
+import numpy as np
+
+
+def estimate_background(array, max_iter=10, force_clipping=False, axis=None):
+    """
+    Estimates the background sky level based on an iterative 3-sigma clipping algorithm. In principle the data are
+    iterativley clipped around the median. At each iteration the mean of the clipped histogram is calculated. If the
+    change from one iteration to the next is less than 1%, estimates for the background and standard deviation in the
+    background are returned. Here, we return the mean if the last iteration did not show more than 20% relative change
+    compared to the fist iteration. Otherwise, the field is assumed to be crowded and the mode is estimated with
+    2.5 * median - 1.5 * mean (see SExtractor doc). Ultimatley at the 'max_iter' iteration, the mode estimate is
+    always returned.
+
+    Parameters
+    ----------
+    array : np.ndarray
+        Input data
+    max_iter : int, optional
+        Maximum iterations. If convergence is not reached, return an estimate of the background
+    force_clipping : bool, optional
+        If set, then even without convergence, the result will be returned after max_iter.
+    axis : int, tuple, optional
+        The axis along which the sky background should be analaysed
+
+    Returns
+    -------
+    tuple
+        Sky background and sky sigma estimates
+
+    """
+
+    # Check for integer data
+    if "int" in str(array.dtype).lower():
+        raise TypeError("integer data not supported")
+
+    masked, idx, sky_save, sky_ini = array.copy(), 0, None, None
+    while True:
+
+        # 3-sigma clip each plane around the median
+        med = np.nanmedian(masked, axis=axis)
+        std = np.nanstd(masked, axis=axis)
+
+        # Expand dimensions if required
+        if axis is not None:
+
+            # If its an integer, we just expand once
+            if isinstance(axis, int):
+                med = np.expand_dims(med, axis=axis)
+                std = np.expand_dims(std, axis=axis)
+
+            # If it's a tuple, we need to iteratively add axes
+            if isinstance(axis, tuple):
+                for i in axis:
+                    med = np.expand_dims(med, axis=i)
+                    std = np.expand_dims(std, axis=i)
+
+        # Mask everything outside 3-sigma around the median in each plane
+        with np.errstate(invalid="ignore"):
+            masked[(masked < med - 3 * std) | (masked > med + 3 * std)] = np.nan
+
+        # Estimate the sky and sigma with the mean of the clipped histogram
+        sky, skysig = np.nanmean(masked, axis=axis), np.nanstd(masked, axis=axis)
+
+        # Save this value in first iteration
+        if idx == 0:
+            sky_ini = sky.copy()
+
+        # Only upon the second iteration we evaluate
+        elif idx > 0:
+
+            # If we have no change within 2% of previous iteration we return
+            if np.mean(np.abs(sky_save / sky - 1)) < 0.02:
+
+                # If (compared to the initial value) the mean has changed by less than 20%, the field is not crowded
+                if np.mean(np.abs(sky / sky_ini - 1)) < 0.2 or force_clipping is True:
+                    return sky, skysig
+
+                # Otherwise the field is crowded and we return an estimate of the mode
+                else:
+                    return 2.5 * np.nanmedian(masked, axis=axis) - 1.5 * np.nanmean(masked, axis=axis), skysig
+
+            # Otherwise we do one more iteration
+            else:
+                pass
+
+        # If we have no convergence after 10 iterations, we return an estimate
+        elif idx > max_iter:
+
+            if force_clipping is True:
+                return sky, skysig
+            else:
+                return 2.5 * np.nanmedian(masked, axis=axis) - 1.5 * np.nanmean(masked, axis=axis), skysig
+
+        # For the iterations between 1 and 10 when no convergence is reached
+        sky_save = sky.copy()
+
+        # Increase loop index
+        idx += 1
