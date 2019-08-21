8cd6ced3458a9c2a239bce72ecd2afbcab36dd34
Stefan Meingast <stefan.meingast@gmail.com>
2019-08-21 09:36:03 +0200

    Updated yaml path

--- vircampype/utils/miscellaneous.py
+++ vircampype/utils/miscellaneous.py
@@ -250,7 +250,7 @@ def str2func(s):
 def read_setup(path_yaml=None):
 
     if path_yaml is None:
-        path_yaml = "/Users/stefan/Dropbox/Projects/vircampype/vircampype/vircampype.yml"
+        path_yaml = "/Users/stefan/Dropbox/Projects/vircampype/vircampype.yml"
 
     # Read YAML
     with open(path_yaml, "r") as stream:
