bf350a587f19757c88769cd2d2050be712ac28af
Stefan Meingast <stefan.meingast@gmail.com>
2019-08-21 11:48:07 +0200

    rdnoise plot and some minor refactoring

--- vircampype/fits/tables/gain.py
+++ vircampype/fits/tables/gain.py
@@ -11,25 +11,52 @@ class MasterGain(MasterTables):
     def __init__(self, file_paths):
         super(MasterGain, self).__init__(file_paths=file_paths)
 
-    _gains = None
+    _gain = None
 
     @property
-    def gains(self):
+    def gain(self):
 
         # Check if already determined
-        if self._gains is not None:
-            return self._gains
+        if self._gain is not None:
+            return self._gain
 
-        self._gains = self.get_column(column_name="gain")
-        return self._gains
+        self._gain = self.get_column(column_name="gain")
+        return self._gain
+
+    _rdnoise = None
+
+    @property
+    def rdnoise(self):
+
+        # Check if already determined
+        if self._rdnoise is not None:
+            return self._rdnoise
+
+        self._rdnoise = self.get_column(column_name="rdnoise")
+        return self._rdnoise
 
     def qc_plot_gain(self, paths=None, axis_size=5):
 
         # Generate path for plots
-        if paths is None:
-            paths = [x.replace(".tab", ".pdf") for x in self.full_paths]
+        paths = self._make_plot_paths(paths=paths)
 
         # Loop over files and create plots
-        for gain, path in zip(self.gains, paths):
+        for gain, path in zip(self.gain, paths):
             plot_value_detector(values=gain, path=path, ylabel="Gain (e-/ADU)",
                                 axis_size=axis_size, overwrite=self.setup["misc"]["overwrite"])
+
+    def qc_plot_rdnoise(self, paths=None, axis_size=5):
+
+        # Generate path for plots
+        paths = self._make_plot_paths(paths=paths)
+
+        # Loop over files and create plots
+        for rdn, path in zip(self.rdnoise, paths):
+            plot_value_detector(values=rdn, path=path, ylabel="Gain (e-/ADU)",
+                                axis_size=axis_size, overwrite=self.setup["misc"]["overwrite"])
+
+    def _make_plot_paths(self, paths):
+        if paths is None:
+            return [x.replace(".tab", ".pdf") for x in self.full_paths]
+        else:
+            return paths
