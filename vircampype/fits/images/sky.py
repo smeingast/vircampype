3163b23021f1bce06ec821b39c36ba58855eb8a2
Stefan Meingast <stefan.meingast@gmail.com>
2019-08-22 14:23:54 +0200

    Added SkyImages superclass to science/offset/std

--- vircampype/fits/images/sky.py
+++ vircampype/fits/images/sky.py
@@ -3,19 +3,25 @@
 from vircampype.fits.images.common import FitsImages
 
 
-class ScienceImages(FitsImages):
+class SkyImages(FitsImages):
+
+    def __init__(self, setup, file_paths=None):
+        super(SkyImages, self).__init__(setup=setup, file_paths=file_paths)
+
+
+class ScienceImages(SkyImages):
 
     def __init__(self, setup, file_paths=None):
         super(ScienceImages, self).__init__(setup=setup, file_paths=file_paths)
 
 
-class OffsetImages(FitsImages):
+class OffsetImages(SkyImages):
 
     def __init__(self, setup, file_paths=None):
         super(OffsetImages, self).__init__(setup=setup, file_paths=file_paths)
 
 
-class StdImages(FitsImages):
+class StdImages(SkyImages):
 
     def __init__(self, setup, file_paths=None):
         super(StdImages, self).__init__(setup=setup, file_paths=file_paths)
