63d9121efb3f382090394eeba4117a01cd703de8
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-03 12:42:40 +0200

    Aperture correction calculator

---
+++ vircampype/utils/photometry.py
@@ -0,0 +1,36 @@
+# =========================================================================== #
+# Import
+import warnings
+import numpy as np
+
+from astropy.modeling import models, fitting
+
+
+def get_aperture_correction(diameters, magnitudes, func="Moffat"):
+
+    # Subtract last aperture from mag to obtain aperture correction for each source
+    mag_diff = magnitudes - magnitudes[:, -1][:, np.newaxis]
+
+    # The aperture correction is now the median across all remaining sources
+    mag_apcor = np.nanmedian(mag_diff, axis=0)
+    magerr_apcor = np.nanstd(mag_diff, axis=0)
+
+    # Choose model based on option
+    if func.lower() == "moffat":
+        model_init = models.Moffat1D()
+    elif func.lower() == "gaussian":
+        model_init = models.Gaussian1D()
+    elif func.lower() == "lorentz":
+        model_init = models.Lorentz1D()
+    else:
+        raise ValueError("Requested fit function '{0}' not available.".format(func))
+
+    # Fit profile
+    fit_m = fitting.LevMarLSQFitter()
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="The fit may be unsuccessful")
+        # noinspection PyTypeChecker
+        model = fit_m(model_init, diameters, mag_apcor)
+
+    # Return aperture correction and model
+    return mag_apcor, magerr_apcor, model
