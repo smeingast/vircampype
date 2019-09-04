20952769d3848c680607e3d352ca0f142702e2bd
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-04 12:53:12 +0200

    Added header merge utility and docstrings

--- vircampype/utils/fits.py
+++ vircampype/utils/fits.py
@@ -1,9 +1,26 @@
 # =========================================================================== #
 # Import
+import warnings
 from astropy.io import fits
+from astropy.io.fits.verify import VerifyWarning
 
 
 def make_image_mef(paths_input, path_output, primeheader=None, overwrite=False):
+    """
+    Creates an MEF image file from multiple input image file.
+
+    Parameters
+    ----------
+    paths_input : iterable
+        List of input paths.
+    path_output : str
+        Path of output file.
+    primeheader : fits.Header, optional
+        If set, the primary header for the output file.
+    overwrite : bool, optional
+        Whether an existing file should be overwritten.
+
+    """
 
     if len(paths_input) == 0:
         raise ValueError("No images to combine")
@@ -26,3 +43,38 @@ def make_image_mef(paths_input, path_output, primeheader=None, overwrite=False):
 
     # Write final HDUlist to disk
     hdulist.writeto(path_output, overwrite=overwrite)
+
+
+def merge_headers(path_1, path_2):
+    """
+    Merges header entries of file 2 into file 1, in the sense that every new item in header 2 that is not present in
+    header 1, is copied to file 1. Forces a new write of the fits file in the end (flush).
+
+    Parameters
+    ----------
+    path_1 : str
+        Path of file 1. Where keywords are copied to.
+    path_2 : str
+        Path of file 2. Where keywords are taken from.
+
+    """
+
+    # Get HDUlists for both files
+    with fits.open(path_1, mode="update") as hdulist_1, fits.open(path_2, mode="update") as hdulist_2:
+
+        # Iterate over HDUs
+        for hdu1, hdu2 in zip(hdulist_1, hdulist_2):
+
+            keys1 = list(hdu1.header.keys())
+
+            # Iterate over every item in 2
+            for key2, val2 in hdu2.header.items():
+
+                # If not in header 1, put there, but ignore HIERARCH warnings
+                if key2 not in keys1:
+                    with warnings.catch_warnings():
+                        warnings.filterwarnings("ignore", category=VerifyWarning)
+                        hdu1.header[key2] = val2
+
+        # Flush changes to first file
+        hdulist_1.flush()
