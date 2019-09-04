92b3132e6830809e0bca165069f25f31776f34d9
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-04 09:40:34 +0200

    Function to create MEF image file from multiple standalone files

---
+++ vircampype/utils/fits.py
@@ -0,0 +1,25 @@
+# =========================================================================== #
+# Import
+from astropy.io import fits
+
+
+def make_image_mef(paths_input, path_output, primeheader=None, overwrite=False):
+
+    # Create empty HDUlist
+    hdulist = fits.HDUList()
+
+    # Make Primary header
+    if primeheader is None:
+        primeheader = fits.Header()
+
+    # Put primary HDU
+    hdulist.append(fits.PrimaryHDU(header=primeheader))
+
+    # Construct image HDUs from input
+    for pi in paths_input:
+
+        with fits.open(pi) as file:
+            hdulist.append(fits.ImageHDU(data=file[0].data, header=file[0].header))
+
+    # Write final HDUlist to disk
+    hdulist.writeto(path_output, overwrite=overwrite)
