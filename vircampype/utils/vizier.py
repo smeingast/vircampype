1c5dd3aaa256f72b5f951ca65f7fb20db9248c2c
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-05 14:10:08 +0200

    vizier tools

---
+++ vircampype/utils/vizier.py
@@ -0,0 +1,21 @@
+# =========================================================================== #
+# Import packages
+from astropy.units import Unit
+from astroquery.vizier import Vizier
+from astropy.coordinates import SkyCoord
+
+
+def download_2mass(lon, lat, radius):
+
+    # Skycoord
+    sc = SkyCoord(lon * Unit("deg"), lat * Unit("deg"), frame="icrs")
+
+    # Setup for Vizier
+    v = Vizier(columns=["*", "+_r"], catalog="II/246/out", row_limit=-1)
+
+    # Submit query
+    result = v.query_region(sc, radius=radius * Unit("deg"), catalog="II/246/out")[0]
+    del result.meta["description"]
+    result.meta["NAME"] = "2MASS"
+
+    return result
