d5bd0091053e7c84c71ac8d222a1bffd38a361b4
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-05 13:07:47 +0200

    Renamed class to PhotometryCatalog

--- vircampype/fits/tables/catalogs.py
+++ vircampype/fits/tables/catalogs.py
@@ -3,7 +3,7 @@
 from vircampype.fits.tables.common import FitsTables
 
 
-class AstrometryCatalog(FitsTables):
+class PhotometryCatalog(FitsTables):
 
     def __init__(self, setup, file_paths=None):
-        super(AstrometryCatalog, self).__init__(file_paths=file_paths, setup=setup)
+        super(PhotometryCatalog, self).__init__(file_paths=file_paths, setup=setup)
