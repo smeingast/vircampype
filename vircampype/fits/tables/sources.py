77e02f177b40bf3ba4497f2c47680d765afd8ee6
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-06 13:03:13 +0200

    added ESOSourceCatalog class

--- vircampype/fits/tables/sources.py
+++ vircampype/fits/tables/sources.py
@@ -9,6 +9,12 @@ class SourceCatalog(FitsTables):
         super(SourceCatalog, self).__init__(file_paths=file_paths, setup=setup)
 
 
+class ESOSourceCatalog(SourceCatalog):
+
+    def __init__(self, setup, file_paths=None):
+        super(ESOSourceCatalog, self).__init__(file_paths=file_paths, setup=setup)
+
+
 class MasterPhotometry(SourceCatalog):
 
     def __init__(self, setup, file_paths=None):
