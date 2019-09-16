76729add099dad5a213350462a63d8e04a4028d9
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-16 17:23:40 +0200

    Increased coadd enlarge factor to 1%

--- vircampype/fits/images/sky.py
+++ vircampype/fits/images/sky.py
@@ -641,13 +641,13 @@ class SkyImages(FitsImages):
             rotation_test = np.linspace(0, np.pi / 2, 360)
             area = []
             for rot in rotation_test:
-                hdr = skycoord2header(skycoord=sc, proj_code="ZEA", rotation=rot, enlarge=1.002,
+                hdr = skycoord2header(skycoord=sc, proj_code="ZEA", rotation=rot, enlarge=1.01,
                                       cdelt=self.setup["astromatic"]["pixel_scale"] / 3600)
                 area.append(hdr["NAXIS1"] * hdr["NAXIS2"])
 
             # Return final header with optimized rotation
             rotation = rotation_test[np.argmin(area)]
-            header_coadd = skycoord2header(skycoord=sc, proj_code="ZEA", rotation=rotation, enlarge=1.002,
+            header_coadd = skycoord2header(skycoord=sc, proj_code="ZEA", rotation=rotation, enlarge=1.01,
                                            cdelt=self.setup["astromatic"]["pixel_scale"] / 3600)
 
             # Dummy check
