86c920ed3ac41e35e0cb2f4cf5c4d82aefcb444e
Stefan Meingast <stefan.meingast@gmail.com>
2019-09-17 10:17:36 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from distutils.core import setup
+
+# Require Python 3
+if sys.version_info < (3, 7):
+    sys.exit("Sorry, Python < 3.7 is not supported")
+
+
+setup(
+    name="vircampype",
+    version="0.1",
+    install_requires=["numpy>=1.17", "scipy>=1.3", "scikit-learn>=0.21", "matplotlib>=3.1", "astropy>=3.1"],
+    url="",
+    license="",
+    author="Stefan Meingast",
+    author_email="stefan.meingast@gmail.com",
+    description="Pipeline for processing VIRCAM data."
+)
