0a5841c766e2dbbafc5d28bb96dbe8ceddf9c1b7
Stefan Meingast <stefan.meingast@gmail.com>
2019-11-20 13:02:31 +0100

    Added QC plot method

--- vircampype/fits/tables/zeropoint.py
+++ vircampype/fits/tables/zeropoint.py
@@ -2,6 +2,7 @@
 # Import
 import numpy as np
 from vircampype.fits.tables.common import MasterTables
+from vircampype.utils.plots import plot_value_detector
 
 
 class MasterZeroPoint(MasterTables):
@@ -83,3 +84,25 @@ class MasterZeroPoint(MasterTables):
 
         """
         return [np.mean(np.stack([tab[name] for name in self._zp_colnames]), axis=0) for tab in self.zp_table]
+
+    @property
+    def zp_mean_std(self):
+        return [np.std(np.stack([tab[name] for name in self._zp_colnames]), axis=0) for tab in self.zp_table]
+
+    def paths_qc_plots(self, paths=None, prefix="zp"):
+        """ Constructs paths for ZP QC plots. """
+        if paths is None:
+            return ["{0}{1}.{2}.pdf".format(self.path_qc_photometry, fp, prefix) for fp in self.file_names]
+        else:
+            return paths
+
+    def qc_plot_zp(self, paths=None, axis_size=5, overwrite=False):
+        """ Generates ZP QC plot. """
+
+        # Generate path for plots
+        paths = self.paths_qc_plots(paths=paths, prefix="zp")
+
+        # Loop over files and create plots
+        for zp, zpstd, path in zip(self.zp_mean, self.zp_mean_std, paths):
+            plot_value_detector(values=zp, errors=zpstd, path=path, ylabel="ZP (mag)", axis_size=axis_size,
+                                overwrite=overwrite, yrange=(np.median(zp) - 0.05, np.median(zp) + 0.05))
