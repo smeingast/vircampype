09099eaed0c61b8fcd6215c4543ad465403e6c7e
Stefan Meingast <stefan.meingast@gmail.com>
2019-12-03 13:56:01 +0100

    Unified final catalog generation for pawprints and tiles

--- vircampype/utils/eso.py
+++ vircampype/utils/eso.py
@@ -5,7 +5,7 @@ from astropy.io import fits
 from vircampype.utils.fitstools import *
 from astropy.coordinates import SkyCoord
 from astropy.stats import sigma_clipped_stats
-from vircampype.utils.miscellaneous import skycoo2visionsid, str2list
+from vircampype.utils.miscellaneous import skycoo2visionsid
 from vircampype.setup import *
 
 
@@ -278,9 +278,6 @@ def make_phase3_pawprints(path_swarped, path_sextractor, outpaths, compressed, a
     hdul_paw[0].header["ASSON1"] = asson_name
     hdul_paw[0].header.set("ASSON1", after="REFERENC")
 
-    # Get aperture indices
-    mag_aper_idx = [[i for i, x in enumerate(apertures_all) if x == b][0] for b in apertures_out]
-
     # Now loop over extensions
     for idx_paw, idx_cat in zip(range(1, 17, 1), range(2, 33, 2)):
 
@@ -290,59 +287,8 @@ def make_phase3_pawprints(path_swarped, path_sextractor, outpaths, compressed, a
         hdr_cat = make_pp_ext_header(hdu_swarped=hdul_swarped[idx_paw], hdu_sex=hdul_sextractor[idx_cat],
                                      fil=hdul_paw[0].header["FILTER"], mode="catalog")
 
-        # Get catalog data for this extension
-        data, sheader = fits.getdata(filename=path_sextractor, ext=idx_cat, header=True)
-
-        # Get difference for first two corrected aperture magnitudes
-        mag_diff12 = data["MAG_APER_1"] - data["MAG_APER_2"]
-        fhwm_stars = np.nanmedian(data["FWHM_WORLD"][data["CLASS_STAR"] > 0.8])
-
-        # Filter bad sources (bad FWHM, and those that have a good mag growth and good FWHM)
-        keep = (mag_diff12 > -0.2) & (data["FWHM_WORLD"] * 3600 > fhwm_stars * 3600 - 0.3)
-
-        # Read aperture magnitudes and aperture corrections
-        mag_aper = [data["MAG_APER"][:, aidx][keep] for aidx in mag_aper_idx]
-        magerr_aper = [data["MAGERR_APER"][:, aidx][keep] for aidx in mag_aper_idx]
-        mag_apc = [data["MAG_APC_{0}".format(idx+1)][keep] for idx in range(len(apertures_out))]
-        mag_zp = [sheader["PYPE MAGZP {0}".format(idx+1)] for idx in range(len(apertures_out))]
-
-        # Compute magnitudes
-        mag_final = [mag + apc + zp for mag, apc, zp in zip(mag_aper, mag_apc, mag_zp)]
-        amag_final = np.array(mag_final)
-
-        # Get bad values
-        amagerr_aper = np.array(magerr_aper)
-        mag_bad = (amag_final > 50.) | (amag_final < 0.) | (amagerr_aper > 2.) | (amagerr_aper < 0.)
-
-        # Mask bad values
-        amag_final[mag_bad], amagerr_aper[mag_bad] = np.nan, np.nan
-
-        # Convert back to lists
-        mag_final, magerr_final = amag_final.tolist(), amagerr_aper.tolist()
-
-        # Create skycoord
-        skycoord = SkyCoord(ra=data["ALPHAWIN_J2000"], dec=data["DELTAWIN_J2000"],
-                            frame="icrs", equinox="J2000", unit="deg")[keep]
-
-        # Create fits columns
-        col_id = fits.Column(name="ID", array=skycoo2visionsid(skycoord=skycoord), format="21A")
-        col_ra = fits.Column(name="RA", array=skycoord.icrs.ra.deg, **kwargs_column_coo)
-        col_dec = fits.Column(name="DEC", array=skycoord.icrs.dec.deg, **kwargs_column_coo)
-        col_fwhm = fits.Column(name="FWHM", array=data["FWHM_WORLD"][keep] * 3600, **kwargs_column_fwhm)
-        col_flags = fits.Column(name="FLAGS", array=data["FLAGS"][keep], **kwargs_column_flags)
-        col_ell = fits.Column(name="ELLIPTICITY", array=data["ELLIPTICITY"][keep], **kwargs_column_el)
-        col_elo = fits.Column(name="ELONGATION", array=data["ELONGATION"][keep], **kwargs_column_el)
-        col_class = fits.Column(name="CLASS", array=data["CLASS_STAR"][keep], **kwargs_column_class)
-
-        cols_mag = []
-        # noinspection PyTypeChecker
-        # TODO: These columns are already in the catalog. No need to create them again
-        for mag, magerr, i in zip(mag_final, magerr_final, range(len(mag_final))):
-
-            cols_mag.append(fits.Column(name="MAG_APER_{0}".format(i+1), array=np.array(mag),
-                                        **kwargs_column_mag))
-            cols_mag.append(fits.Column(name="MAGERR_APER_{0}".format(i+1), array=np.array(magerr),
-                                        **kwargs_column_mag))
+        # Get table colums from pipeline catalog
+        final_cols = _phase3_sex2col(path_sextractor=path_sextractor, extension=idx_cat)
 
         # Remove keywords from header
         for kw in ["ORIGFILE"]:
@@ -351,8 +297,7 @@ def make_phase3_pawprints(path_swarped, path_sextractor, outpaths, compressed, a
 
         # Make final HDUs
         hdu_paw = fits.ImageHDU(data=hdul_swarped[idx_paw].data, header=hdr_paw)
-        hdu_cat = fits.BinTableHDU.from_columns([col_id, col_ra, col_dec] + cols_mag +
-                                                [col_fwhm, col_flags, col_ell, col_elo, col_class], header=hdr_cat)
+        hdu_cat = fits.BinTableHDU.from_columns(final_cols, header=hdr_cat)
 
         # Add HDUs to HDULists
         hdul_paw.append(hdu=hdu_paw)
@@ -397,8 +342,42 @@ def make_phase3_tile(path_swarped, path_sextractor, paths_prov, outpath, compres
     # Add extension name
     exhdr_cat["EXTNAME"] = os.path.basename(outpath)
 
+    # Get table colums from pipeline catalog
+    final_cols = _phase3_sex2col(path_sextractor=path_sextractor, extension=2)
+
+    # Make final HDUs
+    hdul_img = fits.PrimaryHDU(data=hdul_img[0].data, header=prhdr_img)
+    hdul_cat = fits.HDUList(fits.PrimaryHDU(header=prhdr_cat))
+    hdul_cat.append(fits.BinTableHDU.from_columns(final_cols, header=exhdr_cat))
+
+    # Return HDU lists if outpaths are not set
+    hdul_img.writeto(outpath, overwrite=False, checksum=True)
+    hdul_cat.writeto(outpath.replace(".fits", ".cat.fits"), overwrite=False, checksum=True)
+
+
+def _phase3_sex2col(path_sextractor, extension):
+    """
+    Reads a sextractor catalog as generated by the pipeline and returns the final FITS columns in a list.
+
+    Parameters
+    ----------
+    path_sextractor : str
+        Path to sextractor catalog.
+    extension : int
+        Extension in FITS file.
+
+    Returns
+    -------
+    iterable
+        List of FITS columns.
+
+    """
+
     # Get catalog data
-    data, sheader = fits.getdata(filename=path_sextractor, ext=2, header=True)
+    data, sheader = fits.getdata(filename=path_sextractor, ext=extension, header=True)
+
+    # Get aperture indices
+    mag_aper_idx = [[i for i, x in enumerate(apertures_all) if x == b][0] for b in apertures_out]
 
     # Get difference for first two corrected aperture magnitudes
     mag_diff12 = data["MAG_APER_1"] - data["MAG_APER_2"]
@@ -407,49 +386,46 @@ def make_phase3_tile(path_swarped, path_sextractor, paths_prov, outpath, compres
     # Filter bad sources (bad FWHM, and those that have a good mag growth and good FWHM)
     keep = (mag_diff12 > -0.2) & (data["FWHM_WORLD"] * 3600 > fhwm_stars * 3600 - 0.3)
 
-    # Get aperture indices
-    mag_aper_idx = [[i for i, x in enumerate(apertures_all) if x == b][0] for b in apertures_out]
-
     # Read aperture magnitudes and aperture corrections
     mag_aper = [data["MAG_APER"][:, aidx][keep] for aidx in mag_aper_idx]
     magerr_aper = [data["MAGERR_APER"][:, aidx][keep] for aidx in mag_aper_idx]
-    mag_apc = [data["MAG_APC_{0}".format(a)][keep] for a in apertures_out]
-    mag_zp = [sheader["PYPE MAGZP {0}".format(i + 1)] for i in range(len(apertures_out))]
+    mag_apc = [data["MAG_APC_{0}".format(idx + 1)][keep] for idx in range(len(apertures_out))]
+    mag_zp = [sheader["PYPE MAGZP {0}".format(idx + 1)] for idx in range(len(apertures_out))]
 
-    # Comput magnitudes
-    mags_final = [mag + apc + zp for mag, apc, zp in zip(mag_aper, mag_apc, mag_zp)]
-    amag_final = np.array(mags_final)
-    mag_bad = (amag_final > 50.) | (amag_final < 0.)
-    amag_final[mag_bad] = np.nan
-    mags_final = amag_final.tolist()
+    # Compute magnitudes
+    mag_final = [mag + apc + zp for mag, apc, zp in zip(mag_aper, mag_apc, mag_zp)]
+    amag_final = np.array(mag_final)
+
+    # Get bad values
+    amagerr_aper = np.array(magerr_aper)
+    mag_bad = (amag_final > 50.) | (amag_final < 0.) | (amagerr_aper > 2.) | (amagerr_aper < 0.)
+
+    # Mask bad values
+    amag_final[mag_bad], amagerr_aper[mag_bad] = np.nan, np.nan
+
+    # Convert back to lists
+    mag_final, magerr_final = amag_final.tolist(), amagerr_aper.tolist()
 
     # Create skycoord
-    skycoord = SkyCoord(ra=data["ALPHAWIN_J2000"][keep], dec=data["DELTAWIN_J2000"][keep],
-                        frame="icrs", equinox="J2000", unit="deg")
+    skycoord = SkyCoord(ra=data["ALPHAWIN_J2000"], dec=data["DELTAWIN_J2000"],
+                        frame="icrs", equinox="J2000", unit="deg")[keep]
 
     # Create fits columns
     col_id = fits.Column(name="ID", array=skycoo2visionsid(skycoord=skycoord), format="21A")
-    col_ra = fits.Column(name="RA", array=skycoord.icrs.ra.deg, format="D")
-    col_dec = fits.Column(name="DEC", array=skycoord.icrs.dec.deg, format="D")
-    col_fwhm = fits.Column(name="FWHM", array=data["FWHM_WORLD"][keep] * 3600, format="E")
-    col_flags = fits.Column(name="FLAGS", array=data["FLAGS"][keep], format="I")
-    col_ell = fits.Column(name="ELLIPTICITY", array=data["ELLIPTICITY"][keep], format="E")
-    col_elo = fits.Column(name="ELONGATION", array=data["ELONGATION"][keep], format="E")
-    col_class = fits.Column(name="CLASS", array=data["CLASS_STAR"][keep], format="E")
+    col_ra = fits.Column(name="RA", array=skycoord.icrs.ra.deg, **kwargs_column_coo)
+    col_dec = fits.Column(name="DEC", array=skycoord.icrs.dec.deg, **kwargs_column_coo)
+    col_fwhm = fits.Column(name="FWHM", array=data["FWHM_WORLD"][keep] * 3600, **kwargs_column_fwhm)
+    col_flags = fits.Column(name="FLAGS", array=data["FLAGS"][keep], **kwargs_column_flags)
+    col_ell = fits.Column(name="ELLIPTICITY", array=data["ELLIPTICITY"][keep], **kwargs_column_el)
+    col_elo = fits.Column(name="ELONGATION", array=data["ELONGATION"][keep], **kwargs_column_el)
+    col_class = fits.Column(name="CLASS", array=data["CLASS_STAR"][keep], **kwargs_column_class)
 
     cols_mag = []
     # noinspection PyTypeChecker
-    for mag, magerr, i in zip(mags_final, magerr_aper, range(len(mags_final))):
-        cols_mag.append(fits.Column(name="MAG_APER_{0}".format(i + 1), array=np.array(mag), format="E"))
-        cols_mag.append(fits.Column(name="MAGERR_APER_{0}".format(i + 1), array=np.array(magerr), format="E"))
+    # TODO: These columns are already in the catalog. No need to create them again
+    for mag, magerr, i in zip(mag_final, magerr_final, range(len(mag_final))):
+        cols_mag.append(fits.Column(name="MAG_APER_{0}".format(i + 1), array=np.array(mag), **kwargs_column_mag))
+        cols_mag.append(fits.Column(name="MAGERR_APER_{0}".format(i + 1), array=np.array(magerr), **kwargs_column_mag))
 
-    # Make final HDUs
-    hdul_img = fits.PrimaryHDU(data=hdul_img[0].data, header=prhdr_img)
-    hdul_cat = fits.HDUList(fits.PrimaryHDU(header=prhdr_cat))
-    hdul_cat.append(fits.BinTableHDU.from_columns([col_id, col_ra, col_dec] + cols_mag +
-                                                  [col_fwhm, col_flags, col_ell, col_elo, col_class],
-                                                  header=exhdr_cat))
-
-    # Return HDU lists if outpaths are not set
-    hdul_img.writeto(outpath, overwrite=False, checksum=True)
-    hdul_cat.writeto(outpath.replace(".fits", ".cat.fits"), overwrite=False, checksum=True)
+    # Put all table columns together abd return
+    return [col_id, col_ra, col_dec] + cols_mag + [col_fwhm, col_flags, col_ell, col_elo, col_class]
