605c1ad5037f3a65b5fbc2a1e5868d9c52f1315f
Stefan Meingast <stefan.meingast@gmail.com>
2019-12-03 13:21:04 +0100

    Added apertures to hard coded setup

--- vircampype/setup/__init__.py
+++ vircampype/setup/__init__.py
@@ -1,9 +1,18 @@
 
-__all__ = ["kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags", "kwargs_column_el", "kwargs_column_fwhm",
-           "kwargs_column_class"]
+__all__ = ["apcor_diam_eval", "apcor_diam_save", "kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags",
+           "kwargs_column_el", "kwargs_column_fwhm", "kwargs_column_class"]
+
+# =========================================================================== #
+# Photometry
+# =========================================================================== #
+apcor_diam_eval = 0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, \
+                  5.0, 5.5, 6.0, 7.0, 8.0, 9.0, 10.0, 12.0, 13.5, 15.0, 17.5, 20.0
+apcor_diam_save = 2.0, 3.0, 6.0, 9.0, 12.0, 15.0
+
 
 # =========================================================================== #
 # Table format
+# =========================================================================== #
 kwargs_column_mag = {"format": "1E", "disp": "F8.4", "unit": "mag"}
 kwargs_column_coo = {"format": "1D", "disp": "F11.7", "unit": "deg"}
 kwargs_column_flags = {"format": "1I", "disp": "I3"}
