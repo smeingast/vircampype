b0260dec22865cbdbd15ee06b19cefe6aae60205
Stefan Meingast <stefan.meingast@gmail.com>
2020-10-29 17:50:04 +0100

    Removed data setup and added it to instance setup

--- vircampype/setup/__init__.py
+++ vircampype/setup/__init__.py
@@ -1,16 +1,6 @@
 
-__all__ = ["apertures", "kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags",
-           "kwargs_column_el", "kwargs_column_fwhm", "kwargs_column_class", "saturate_vircam", "fpa_layout"]
-
-# =========================================================================== #
-# Photometry
-# =========================================================================== #
-apertures = [3.0, 6.0, 9.0, 12.0, 15.0]
-
-saturate_vircam = [33000, 32000, 33000, 32000, 24000, 24000, 35000, 33000,
-                   35000, 35000, 37000, 34000, 33000, 35000, 34000, 34000]
-
-fpa_layout = [4, 4]
+__all__ = ["kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags",
+           "kwargs_column_el", "kwargs_column_fwhm", "kwargs_column_class"]
 
 # =========================================================================== #
 # Table format
