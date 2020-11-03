5de005f79feb5cb257902194db036cf162c4e2d3
Stefan Meingast <stefan.meingast@gmail.com>
2020-11-03 10:18:29 +0100

    Column units now parsed from astropy unit module

--- vircampype/setup/__init__.py
+++ vircampype/setup/__init__.py
@@ -1,3 +1,5 @@
+from astropy.units import Unit
+
 
 __all__ = ["kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags",
            "kwargs_column_el", "kwargs_column_fwhm", "kwargs_column_class"]
@@ -5,9 +7,9 @@ __all__ = ["kwargs_column_mag", "kwargs_column_coo", "kwargs_column_flags",
 # =========================================================================== #
 # Table format
 # =========================================================================== #
-kwargs_column_mag = {"format": "1E", "disp": "F8.4", "unit": "mag"}
-kwargs_column_coo = {"format": "1D", "disp": "F11.7", "unit": "deg"}
+kwargs_column_mag = {"format": "1E", "disp": "F8.4", "unit": Unit("mag")}
+kwargs_column_coo = {"format": "1D", "disp": "F11.7", "unit": Unit("deg")}
 kwargs_column_flags = {"format": "1I", "disp": "I3"}
 kwargs_column_el = {"format": "1E", "disp": "F8.3"}
-kwargs_column_fwhm = {"format": "1E", "disp": "F7.4", "unit": "arcsec"}
+kwargs_column_fwhm = {"format": "1E", "disp": "F7.4", "unit": Unit("arcsec")}
 kwargs_column_class = {"format": "1E", "disp": "F6.3"}
