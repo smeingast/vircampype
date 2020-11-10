bd2f594a9cdd6ca50abebc1266d2d21d412877be
Stefan Meingast <stefan.meingast@gmail.com>
2020-11-10 17:14:18 +0100

    Forcing kernel size of 2 pixels in aperture correction maps

--- vircampype/fits/tables/sextractor.py
+++ vircampype/fits/tables/sextractor.py
@@ -327,7 +327,8 @@ class SextractorCatalogs(SourceCatalogs):
                     # Grid
                     apc_grid = grid_value_2d(x=xx[mask], y=yy[mask], value=mag[mask], x_min=0, y_min=0,
                                              x_max=hdr["NAXIS1"], y_max=hdr["NAXIS2"], nx=n_bins_x,
-                                             ny=n_bins_y, conv=True, weights=weights[mask], upscale=False)
+                                             ny=n_bins_y, conv=True, weights=weights[mask], upscale=False,
+                                             kernel_size=2)
 
                     # Rescale to given size
                     apc_grid = np.array(Image.fromarray(apc_grid).resize(size=output_size, resample=Image.LANCZOS))
