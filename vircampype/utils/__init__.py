877e5a543ff5fdd9494bcd01c2a369eec8bb6d1e
Stefan Meingast <stefan.meingast@gmail.com>
2020-11-27 14:35:09 +0100

    Removed all imports

--- vircampype/utils/__init__.py
+++ vircampype/utils/__init__.py
@@ -1,12 +1,1 @@
-from .wcs import *
-from .eso import *
-from .math import *
-from .plots import *
-from .system import *
-from .vizier import *
-from .fitstools import *
-from .exceptions import *
-from .astromatic import *
-from .photometry import *
-from .miscellaneous import *
 
