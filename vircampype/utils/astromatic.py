1a99e944426390e6435cd59ab6a16f1e3f3a681d
Stefan Meingast <stefan.meingast@gmail.com>
2020-11-27 14:49:46 +0100

    moved yml2config to system.py

--- vircampype/utils/astromatic.py
+++ vircampype/utils/astromatic.py
@@ -6,54 +6,7 @@ from astropy.io.fits.header import Header
 from vircampype.utils.system import read_setup
 
 # Define objects in this module
-__all__ = ["yml2config", "read_scamp_header", "replace_astrometry", "sextractor2imagehdr"]
-
-
-def yml2config(path, skip=None, **kwargs):
-    """
-    Reads a YML file at a given path and converts the entries to a string that can be passed to astromatic tools.
-
-    Parameters
-    ----------
-    path : str
-        Path to YML file.
-    skip : list, optional
-        If set, ignore the given keywords in the list
-    kwargs
-        Any available setup parameter can be overwritten (e.g. catalog_name="catalog.fits")
-
-    Returns
-    -------
-    str
-        Full string constructed from YML setup.
-
-    """
-
-    setup = read_setup(path_yaml=path)
-
-    # Loop over setup and construct command
-    s = ""
-    for key, val in setup.items():
-
-        # Skip if set
-        if skip is not None:
-            if key.lower() in [s.lower() for s in skip]:
-                continue
-
-        # Convert key to lower case
-        key = key.lower()
-
-        # Strip any whitespace
-        if isinstance(val, str):
-            val = val.replace(" ", "")
-
-        # Overwrite with kwargs
-        if key in kwargs:
-            s += "-{0} {1} ".format(key.upper(), kwargs[key])
-        else:
-            s += "-{0} {1} ".format(key.upper(), val)
-
-    return s
+__all__ = ["read_scamp_header", "replace_astrometry", "sextractor2imagehdr"]
 
 
 def read_scamp_header(path, remove_pv=False):
--- vircampype/utils/system.py
+++ vircampype/utils/system.py
@@ -10,7 +10,7 @@ from itertools import zip_longest
 
 # Define objects in this module
 __all__ = ["run_cmds", "run_command_bash", "module_exists", "which", "read_setup", "remove_file", "copy_file",
-           "make_folder"]
+           "make_folder", "yml2config"]
 
 
 def run_cmds(cmds, n_processes=1, silent=True):
@@ -135,3 +135,50 @@ def copy_file(a, b):
 def make_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
+
+
+def yml2config(path, skip=None, **kwargs):
+    """
+    Reads a YML file at a given path and converts the entries to a string that can be passed to astromatic tools.
+
+    Parameters
+    ----------
+    path : str
+        Path to YML file.
+    skip : list, optional
+        If set, ignore the given keywords in the list
+    kwargs
+        Any available setup parameter can be overwritten (e.g. catalog_name="catalog.fits")
+
+    Returns
+    -------
+    str
+        Full string constructed from YML setup.
+
+    """
+
+    setup = read_setup(path_yaml=path)
+
+    # Loop over setup and construct command
+    s = ""
+    for key, val in setup.items():
+
+        # Skip if set
+        if skip is not None:
+            if key.lower() in [s.lower() for s in skip]:
+                continue
+
+        # Convert key to lower case
+        key = key.lower()
+
+        # Strip any whitespace
+        if isinstance(val, str):
+            val = val.replace(" ", "")
+
+        # Overwrite with kwargs
+        if key in kwargs:
+            s += "-{0} {1} ".format(key.upper(), kwargs[key])
+        else:
+            s += "-{0} {1} ".format(key.upper(), val)
+
+    return s
