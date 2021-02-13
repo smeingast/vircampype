082395eb6549acb08065ec50132a4ae6393b3ed3
Stefan Meingast <stefan.meingast@gmail.com>
2021-02-13 14:16:55 +0100

    Method to build CrA header

---
+++ vircampype/visions/build_field_header_cra.py
@@ -0,0 +1,20 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Set paths
+path_base = "/Volumes/Data/VISIONS/198C-2009A/data_wide/"
+
+# Find files
+files = glob.glob(path_base + "CrA*/**/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="dummy",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             n_jobs=6)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+images.build_coadd_header()
