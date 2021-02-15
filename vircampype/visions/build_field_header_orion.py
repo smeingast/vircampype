7351adea3df849ca588718fbfc15674238eb294f
Stefan Meingast <stefan.meingast@gmail.com>
2021-02-15 17:36:18 +0100

    Building field header for Orion

---
+++ vircampype/visions/build_field_header_orion.py
@@ -0,0 +1,19 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Find files
+path_base = "/Volumes/Data/VISIONS/198C-2009F/data_wide/"
+files = glob.glob(path_base + "Orion*/A/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="Orion_wide",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             projection=None)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Build header
+images.build_coadd_header()
