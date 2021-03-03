95d355ef7ecb1ddc9b49f0ace57514c9e49ebb08
Stefan Meingast <stefan.meingast@gmail.com>
2021-03-03 15:17:55 +0100

    Headers for Lupus control fields N and S

---
+++ vircampype/visions/build_field_header_lupus_control_s.py
@@ -0,0 +1,19 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Find files
+path_base = "/Volumes/Data/VISIONS/198C-2009E/data_control/"
+files = glob.glob(path_base + "Lupus_control_S*/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="Lupus_control_s",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             projection=None)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Build header
+images.build_coadd_header()
