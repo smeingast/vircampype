60a7d809929136e1123a62bf99db53fe515a5af5
Stefan Meingast <stefan.meingast@gmail.com>
2021-03-03 17:09:09 +0100

    Control field header for the Pipe

---
+++ vircampype/visions/build_field_header_pipe_control.py
@@ -0,0 +1,19 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Find files
+path_base = "/Volumes/Data/VISIONS/198C-2009H/data_control/"
+files = glob.glob(path_base + "Pipe_control*/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="Pipe_control",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             projection=None)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Build header
+images.build_coadd_header()
