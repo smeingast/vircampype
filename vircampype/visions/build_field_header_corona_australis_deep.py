cf2486ea6ba519cec81854fa66b43de59848b1b0
Stefan Meingast <stefan.meingast@gmail.com>
2021-03-03 15:29:50 +0100

    Header for CrA deep field

---
+++ vircampype/visions/build_field_header_corona_australis_deep.py
@@ -0,0 +1,22 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Set paths
+path_base = "/Volumes/Data/VISIONS/198C-2009E/data_deep/"
+files = glob.glob(path_base + "CrA*/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="CrA_deep",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             n_jobs=6)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Split science images
+science = images.split_types()["science"]
+
+# Build header
+science.build_coadd_header()
