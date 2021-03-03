41612e2f9d7a6f816407a592a6b00ad906241551
Stefan Meingast <stefan.meingast@gmail.com>
2021-03-03 14:49:30 +0100

    Function to build common header for control field

---
+++ vircampype/visions/build_field_header_ophiuchus_control.py
@@ -0,0 +1,22 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Find files
+path_base = "/Volumes/Data/VISIONS/198C-2009A/data_control/"
+files = glob.glob(path_base + "*/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="Ophiuchus_control",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+             projection=None)
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Split science images
+science = images.split_types()["science"]
+
+# Build header
+science.build_coadd_header()
