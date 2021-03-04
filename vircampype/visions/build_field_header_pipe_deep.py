0c41d77eb2fa4677f077608682cba0b75fe973c6
Stefan Meingast <stefan.meingast@gmail.com>
2021-03-04 10:26:57 +0100

    Field headers for Pipe deep

---
+++ vircampype/visions/build_field_header_pipe_deep.py
@@ -0,0 +1,21 @@
+# Import
+import glob
+from vircampype.fits.images.sky import RawScienceImages
+
+# Set paths
+path_base = "/Volumes/Data/VISIONS/198C-2009H/data_deep/"
+files = glob.glob(path_base + "Pipe*/*.fits")
+
+# Set dummy pipeline setup
+setup = dict(name="Pipe_deep",
+             path_data="/dev/null",
+             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/")
+
+# Instantiate files
+images = RawScienceImages(setup=setup, file_paths=files)
+
+# Split science images
+science = images.split_types()["science"]
+
+# Build header
+science.build_coadd_header()
