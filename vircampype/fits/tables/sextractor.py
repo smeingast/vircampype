e9a8cc179d648e8d3af503aa13cf2a97bab1a8b1
Stefan Meingast <stefan.meingast@gmail.com>
2021-05-13 09:11:03 +0200

    Removed obsolete method

--- vircampype/fits/tables/sextractor.py
+++ vircampype/fits/tables/sextractor.py
@@ -694,33 +694,6 @@ class PhotometricCalibratedSextractorCatalogs(AstrometricCalibratedSextractorCat
     def __init__(self, setup, file_paths=None):
         super(PhotometricCalibratedSextractorCatalogs, self).__init__(file_paths=file_paths, setup=setup)
 
-    def write_coadd_flux_scale(self):
-        """ Constructs flux scale for coaddition from different zero points across all images and detectors. """
-
-        # Convert ZPs to flux scaling factor
-        zps = np.array(self.read_from_data_headers(keywords=["HIERARCH PYPE ZP MAG_AUTO"])[0])
-        # flx_scale = (10**((zps - self.setup.target_zp) / 2.5)).tolist()
-
-        # TODO Should be like this, but produces worse results
-        flx_scale = (10**((self.setup.target_zp - zps) / 2.5)).tolist()
-
-        # Loop over files and write to disk
-        for idx_file in range(self.n_files):
-
-            # Create output path
-            outpath = self.paths_full[idx_file].replace(".full.fits.ctab", ".ahead")
-
-            # Check if the file is already there and skip if it is
-            if check_file_exists(file_path=outpath, silent=self.setup.silent) and not self.setup.overwrite:
-                continue
-
-            # Create string to write from all scaling factor for current file
-            s = "\n".join(["FLXSCALE= {0:0.5f}\nEND".format(x) for x in flx_scale[idx_file]]) + "\n"
-
-            # Write to disk
-            with open(outpath, "w") as file:
-                file.write(s)
-
     def paths_qc_plots(self, paths, prefix=""):
 
         if paths is None:
