d12570bf9b5b6c7703d99f56040e31fc0bce5b31
Stefan Meingast <stefan.meingast@gmail.com>
2021-06-08 15:09:31 +0200

    Added more masks for CrA deep

--- vircampype/visions/sourcemasks.py
+++ vircampype/visions/sourcemasks.py
@@ -24,16 +24,17 @@ class CoronaAustralisDeepSourceMasks(SourceMasks):
     def __init__(self):
 
         # Define masks (ra, dec, size)
-        m1 = (284.89, -36.63, 500)
-        m2 = (285.48, -36.96, 250)
-        m3 = (285.43, -36.97, 250)
+        m1 = (284.89, -36.63, 600)
+        m2 = (285.48, -36.96, 300)
+        m3 = (285.43, -36.97, 300)
         m4 = (285.42, -36.88, 300)
         m5 = (285.40, -37.01, 200)
         m6 = (285.29, -36.96, 200)
         m7 = (285.84, -37.29, 200)
+        m8 = (285.31, -36.96, 150)
 
         # Put in list
-        masks_all = [m1, m2, m3, m4, m5, m6, m7]
+        masks_all = [m1, m2, m3, m4, m5, m6, m7, m8]
 
         # Call parent
         super(CoronaAustralisDeepSourceMasks, self).__init__(*list(zip(*masks_all)))
