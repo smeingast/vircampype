007b74073d8b21b446914963b90a368193a4564e
Stefan Meingast <stefan.meingast@gmail.com>
2021-08-02 15:27:05 +0200

    Updated scripts with some more pipeline setup attributes

--- vircampype/visions/setups/write_setup_cra_vhs.py
+++ vircampype/visions/setups/write_setup_cra_vhs.py
@@ -17,7 +17,7 @@ unique_directories_j = sorted(list(set([os.path.dirname(x) + "/" for x in files_
 unique_directories_ks = sorted(list(set([os.path.dirname(x) + "/" for x in files_ks])))
 
 # Reference limits
-reference_mag_lim_j = 13.0, 16.0
+reference_mag_lim_j = 13.0, 15.5
 reference_mag_lim_ks = 12.0, 15.0
 
 # Numer of parallel jobs
@@ -27,13 +27,18 @@ n_jobs = 16
 projection = "Corona_Australis_wide"
 additional_source_masks = "Corona_Australis_wide"
 
+# Archive
+archive = True
+
+# Headers
+external_headers = True
 
 # Generate setups and write them to disk
 for udj in unique_directories_j:
     name = "{0}_{1}".format(udj.split("data_vhs/")[1].split("/")[0], "J")
-    setup = dict(name=name, path_data=udj, path_pype=path_pype, n_jobs=n_jobs,
-                 reference_mag_lim=reference_mag_lim_j, projection=projection,
-                 additional_source_masks=additional_source_masks)
+    setup = dict(name=name, path_data=udj, path_pype=path_pype, n_jobs=n_jobs, reference_mag_lim=reference_mag_lim_j,
+                 projection=projection, additional_source_masks=additional_source_masks, archive=archive,
+                 external_headers=external_headers)
 
     # Write YML
     path_yml = "{0}{1}.yml".format(path_scripts, name)
@@ -43,9 +48,9 @@ for udj in unique_directories_j:
 
 for udks in unique_directories_ks:
     name = "{0}_{1}".format(udks.split("data_vhs/")[1].split("/")[0], "Ks")
-    setup = dict(name=name, path_data=udks, path_pype=path_pype, n_jobs=n_jobs,
-                 reference_mag_lim=reference_mag_lim_ks, projection=projection,
-                 additional_source_masks=additional_source_masks)
+    setup = dict(name=name, path_data=udks, path_pype=path_pype, n_jobs=n_jobs, reference_mag_lim=reference_mag_lim_ks,
+                 projection=projection, additional_source_masks=additional_source_masks, archive=archive,
+                 external_headers=external_headers)
 
     # Write YML
     path_yml = "{0}{1}.yml".format(path_scripts, name)
