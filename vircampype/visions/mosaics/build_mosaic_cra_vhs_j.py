f283f8f0c70b25e7de136deb4fab036e883b330b
Stefan Meingast <stefan.meingast@gmail.com>
2021-09-20 09:04:27 +0200

    Updated file paths

--- vircampype/visions/mosaics/build_mosaic_cra_vhs_j.py
+++ vircampype/visions/mosaics/build_mosaic_cra_vhs_j.py
@@ -2,7 +2,7 @@ from vircampype.visions.mosaics.main import build_mosaic
 
 # Setup for pipeline
 name = "CrA_mosaic_J"
-path_scripts = "/Volumes/Data/VHS/CrA/scripts/J/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VHS/CrA/J/"
 path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/CrA/"
 path_data = f"/Volumes/Data/VHS/CrA/data_mosaic/{name}/"
 path_pype = "/Volumes/Data/VHS/CrA/vircampype/"
@@ -12,6 +12,7 @@ reference_mag_lim = (13.0, 15.5)  # J VHS
 phase3_photerr_internal = 0.005
 n_jobs = 18
 
+# Launch mosaic builder
 build_mosaic(path_master_astro_photo=path_master_astro_photo, path_pype=path_pype, path_data=path_data, n_jobs=n_jobs,
              projection=projection, additional_source_masks=additional_source_masks, path_scripts=path_scripts,
              reference_mag_lim=reference_mag_lim, phase3_photerr_internal=phase3_photerr_internal, name=name)
