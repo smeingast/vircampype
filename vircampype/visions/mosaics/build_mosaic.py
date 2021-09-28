d58f8f77d20de181391d02b16c3ebe979cccf48c
Stefan Meingast <stefan.meingast@gmail.com>
2021-09-28 13:34:40 +0200

    Updated mosaic builder after forcing same ZP for all fields

--- vircampype/visions/mosaics/build_mosaic.py
+++ vircampype/visions/mosaics/build_mosaic.py
@@ -7,8 +7,8 @@ from vircampype.tools.miscellaneous import flat_list
 from vircampype.tools.systemtools import make_folder, make_symlinks, copy_file
 
 
-def build_mosaic(path_master_astro_photo, path_scripts, name, path_data, path_pype, n_jobs, projection,
-                 additional_source_masks, phase3_photerr_internal, reference_mag_lim):
+def build_mosaic(name, path_scripts, path_data, path_pype, path_master_astro_photo, n_jobs, additional_source_masks,
+                 reference_mag_lim, projection, phase3_photerr_internal, **kwargs):
 
     # Check if master catalogs are available
     path_master_astro = "{0}MASTER-ASTROMETRY.fits.tab".format(path_master_astro_photo)
@@ -21,40 +21,35 @@ def build_mosaic(path_master_astro_photo, path_scripts, name, path_data, path_py
     print("Found {0:2d} scripts".format(len(paths_scripts)))
 
     # Loop over scripts
-    field_names, paths_folders_raw, paths_folders_processed_final, paths_folders_master = [], [], [], []
+    field_names, paths_folders_raw, paths_folders_resampled, paths_folders_statistics = [], [], [], []
     for ss in paths_scripts:
         yml = read_yml(ss)
         paths_folders_raw.append(yml["path_data"])
-        paths_folders_processed_final.append(yml["path_pype"] + yml["name"] + "/processed_final/")
-        paths_folders_master.append(yml["path_pype"] + yml["name"] + "/master/")
+        paths_folders_resampled.append(yml["path_pype"] + yml["name"] + "/resampled/")
+        paths_folders_statistics.append(yml["path_pype"] + yml["name"] + "/statistics/")
         field_names.append(yml["name"])
 
     # Pipeline setup
     setup = dict(name=name, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs, projection=projection,
                  additional_source_masks=additional_source_masks, phase3_photerr_internal=phase3_photerr_internal,
                  reference_mag_lim=reference_mag_lim, external_headers=True, build_stacks=False, build_tile=True,
-                 build_phase3=True, archive=False, qc_plots=True)
+                 build_phase3=True, archive=False, qc_plots=True, **kwargs)
 
-    # Find all raw input files
-    paths_data_raw = flat_list([glob.glob(p + "*.fits") for p in paths_folders_raw])
-    links_data_raw = ["{0}{1}".format(path_data, os.path.basename(p)) for p in paths_data_raw]
-    print("Found {0:4d} raw images".format(len(paths_data_raw)))
+    # Find resampled images and create link paths
+    paths_resampled = flat_list([glob.glob(p + "*resamp.fits") for p in paths_folders_resampled])
+    links_resampled = ["{0}{1}/resampled/{2}".format(path_pype, name, os.path.basename(f)) for f in paths_resampled]
+    print("Found {0:4d} resampled images".format(len(paths_resampled)))
 
-    # Find processed images
-    paths_data_proc = flat_list([glob.glob(p + "*.fits") for p in paths_folders_processed_final])
-    print("Found {0:4d} processed images".format(len(paths_data_proc)))
+    # Find resampled weights and create link paths
+    paths_resampled_weights = flat_list([glob.glob(p + "*resamp.weight.fits") for p in paths_folders_resampled])
+    links_resampled_weights = ["{0}{1}/resampled/{2}".format(path_pype, name, os.path.basename(f))
+                               for f in paths_resampled_weights]
+    print("Found {0:4d} resampled weights".format(len(paths_resampled_weights)))
 
-    # Find Source masks
-    paths_source_masks = flat_list([glob.glob(p + "MASTER-SOURCE-MASK*.fits") for p in paths_folders_master])
-    print("Found {0:4d} master source masks".format(len(paths_source_masks)))
-
-    # Find master weights
-    paths_weights = flat_list([glob.glob(p + "MASTER-WEIGHT*.fits") for p in paths_folders_master])
-    print("Found {0:4d} master weights".format(len(paths_weights)))
-
-    # Safety check
-    if len(paths_data_raw) != len(paths_data_proc) != len(paths_source_masks) != len(paths_weights):
-        raise ValueError("Raw and processed images not matching")
+    # Find statistics fikes
+    paths_statistics = flat_list([glob.glob(p + "*.fits") for p in paths_folders_statistics])
+    links_statistics = ["{0}{1}/statistics/{2}".format(path_pype, name, os.path.basename(f)) for f in paths_statistics]
+    print("Found {0:4d} statistics files".format(len(paths_statistics)))
 
     # Require input to continue
     if (input("Continue (Y/n)") or "Y") != "Y":
@@ -75,27 +70,11 @@ def build_mosaic(path_master_astro_photo, path_scripts, name, path_data, path_py
 
     # Create symbolic links for raw files
     print("Creating symbolic links")
-    make_symlinks(paths_data_raw, links_data_raw)
+    make_symlinks(paths_resampled, links_resampled)
+    make_symlinks(paths_resampled_weights, links_resampled_weights)
+    make_symlinks(paths_statistics, links_statistics)
 
-    # Create symbolic links for processed data
-    for fn in field_names:
-
-        # All final processed files
-        ff = glob.glob(path_pype + fn + "/processed_final/*")
-        fl = ["{0}{1}/processed_final/{2}".format(path_pype, name, os.path.basename(f)) for f in ff]
-        make_symlinks(ff, fl)
-
-        # Source masks
-        ff = glob.glob(path_pype + fn + "/master/MASTER-SOURCE-MASK*")
-        fl = ["{0}{1}/master/{2}".format(path_pype, name, os.path.basename(f)) for f in ff]
-        make_symlinks(ff, fl)
-
-        # Weights
-        ff = glob.glob(path_pype + fn + "/master/MASTER-WEIGHT-IMAGE*")
-        fl = ["{0}{1}/master/{2}".format(path_pype, name, os.path.basename(f)) for f in ff]
-        make_symlinks(ff, fl)
-
-    # Set early processing steps to completed
+    # Set pipeline processing steps to completed
     print("Setting pipeline status")
     pipe.status.processed_raw_basic = True
     pipe.status.master_photometry = True
@@ -106,7 +85,10 @@ def build_mosaic(path_master_astro_photo, path_scripts, name, path_data, path_py
     pipe.status.processed_raw_final = True
     pipe.status.master_weight_image = True
     pipe.status.astrometry = True
+    pipe.status.illumcorr = True
+    pipe.status.resampled = True
+    pipe.status.build_statistics = True
 
-    # Start processing
+    # Launch pipeline
     print("Launching pipeline")
     pipe.process_science()
