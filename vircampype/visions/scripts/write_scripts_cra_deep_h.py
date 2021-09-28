22947cf31976af197bcf98b446b042f96ac31511
Stefan Meingast <stefan.meingast@gmail.com>
2021-09-28 13:33:32 +0200

    Pipeline scripts for CrA deep fields

--- vircampype/visions/scripts/write_scripts_cra_deep_h.py
+++ vircampype/visions/scripts/write_scripts_cra_deep_h.py
@@ -4,13 +4,13 @@ from vircampype.visions.scripts.write_scripts import write_scripts
 # Define paths
 path_data = "/Volumes/Data/VISIONS/198C-2009E/data_deep/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/198C-2009E/CrA/CrA_deep_Ks/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/198C-2009E/CrA/CrA_deep_H/"
 
 # Search for files
-paths_files = sorted(glob(path_data + "CrA_deep_Ks*/*.fits"))
+paths_files = sorted(glob(path_data + "CrA_deep_H*/*.fits"))
 
 # Write scripts
 write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=False,
               projection="Corona_Australis_deep", additional_source_masks="Corona_Australis_deep", n_jobs=18,
-              external_headers=True, reference_mag_lim=(11.5, 14.5), phase3_photerr_internal=0.005,
+              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
               name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
