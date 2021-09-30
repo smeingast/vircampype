fd142159e3fb5a4da48cf273a896b0889b5929d6
Stefan Meingast <stefan.meingast@gmail.com>
2021-09-30 15:43:27 +0200

    Updated arguments

--- vircampype/visions/mosaics/build_mosaic_cra_vhs_j.py
+++ vircampype/visions/mosaics/build_mosaic_cra_vhs_j.py
@@ -7,13 +7,10 @@ path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/CrA/"
 path_data = f"/Volumes/Data/VHS/CrA/data_mosaic/{name}/"
 path_pype = "/Volumes/Data/VHS/CrA/vircampype/"
 projection = "Corona_Australis_wide"
-additional_source_masks = "Corona_Australis_wide"
 reference_mag_lim = (13.0, 15.5)  # J VHS
 phase3_photerr_internal = 0.005
 n_jobs = 18
 
-# Launch mosaic builder
 build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
-             path_master_astro_photo=path_master_astro_photo, additional_source_masks=additional_source_masks,
-             reference_mag_lim=reference_mag_lim, projection=projection, build_class_star_library=True,
-             phase3_photerr_internal=phase3_photerr_internal)
+             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
+             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
--- vircampype/visions/mosaics/build_mosaic_cra_wide_c.py
+++ vircampype/visions/mosaics/build_mosaic_cra_wide_c.py
@@ -2,16 +2,15 @@ from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "CrA_mosaic_H_C"
-path_scripts = "/Volumes/Data/VISIONS/198C-2009E/scripts/CrA_wide_C/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/198C-2009E/CrA/CrA_wide_C/"
 path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/CrA/"
-path_data = "/Volumes/Data/VISIONS/198C-2009E/data_mosaic/{0}/".format(name)
+path_data = f"/Volumes/Data/VISIONS/198C-2009E/data_mosaic/{name}/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
 projection = "Corona_Australis_wide"
-additional_source_masks = "Corona_Australis_wide"
 reference_mag_lim = (12.0, 15.0)  # H wide
 phase3_photerr_internal = 0.005
 n_jobs = 18
 
-build_mosaic(path_master_astro_photo=path_master_astro_photo, path_pype=path_pype, path_data=path_data, n_jobs=n_jobs,
-             projection=projection, additional_source_masks=additional_source_masks, path_scripts=path_scripts,
-             reference_mag_lim=reference_mag_lim, phase3_photerr_internal=phase3_photerr_internal, name=name)
+build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
+             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
+             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
