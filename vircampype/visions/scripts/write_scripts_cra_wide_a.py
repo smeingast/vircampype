94203423163288930671b865f4635b10efd7083a
Stefan Meingast <stefan.meingast@gmail.com>
2021-10-01 17:43:54 +0200

    Scripts for CrA epoch A

---
+++ vircampype/visions/scripts/write_scripts_cra_wide_a.py
@@ -0,0 +1,17 @@
+from glob import glob
+from vircampype.visions.scripts.write_scripts import write_scripts
+
+# Define paths
+path_data = "/Volumes/Data/VISIONS/198C-2009A/data_wide/"
+path_pype = "/Volumes/Data/VISIONS/198C-2009A/vircampype/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/198C-2009E/CrA/CrA_wide_A/"
+
+# Search for files
+paths_files = sorted(glob(path_data + "CrA*/A/*.fits"))
+
+# Write scripts
+# reference_mag_lim = dict(J=(12.5, 15.5), H=(12.0, 15.0), Ks=(11.5, 14.5))
+write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=False,
+              projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
+              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
+              name_suffix="_A", build_stacks=True, build_tile=True, build_phase3=True)
