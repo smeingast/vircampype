d0fdfdec4839beba75f75cdd9fa6f262d80331fd
Stefan Meingast <stefan.meingast@gmail.com>
2021-10-04 09:29:19 +0200

    Updated scripts path

--- vircampype/visions/scripts/write_scripts_cra_wide_c.py
+++ vircampype/visions/scripts/write_scripts_cra_wide_c.py
@@ -4,13 +4,12 @@ from vircampype.visions.scripts.write_scripts import write_scripts
 # Define paths
 path_data = "/Volumes/Data/VISIONS/198C-2009E/data_wide/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/198C-2009E/CrA/CrA_wide_C/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_wide_C/"
 
 # Search for files
 paths_files = sorted(glob(path_data + "CrA*/A/*.fits"))
 
 # Write scripts
-# reference_mag_lim = dict(J=(12.5, 15.5), H=(12.0, 15.0), Ks=(11.5, 14.5))
 write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=False,
               projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
               external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
