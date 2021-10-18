5a4e7781fdbfa9ecc6878ea97ecee5830cebe19e
Stefan Meingast <stefan.meingast@gmail.com>
2021-10-18 16:59:09 +0200

    Removed from git

--- vircampype/visions/other/sort_data.py
+++
@@ -1,6 +0,0 @@
-from vircampype.tools.datatools import sort_vircam_calibration, sort_vircam_science
-
-pa = "/Volumes/Data/VISIONS/198C-2009L/"
-pc = "/Volumes/Data/VISIONS/198C-2009L/calibration/"
-sort_vircam_calibration(path_all=pa, path_calibration=pc, extension=".fz")
-sort_vircam_science(path=pa, extension=".fz")
