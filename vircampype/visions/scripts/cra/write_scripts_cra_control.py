63cd6f0449780142c268b9c59dfd02932a58f36e
Stefan Meingast <stefan.meingast@gmail.com>
2021-11-08 12:12:43 +0100

    Set external headers presence to True

--- vircampype/visions/scripts/cra/write_scripts_cra_control.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_control.py
@@ -12,5 +12,5 @@ paths_files = sorted(glob(path_data + "CrA_control*/*.fits"))
 # Write scripts
 write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=False,
               projection="Corona_Australis_control", additional_source_masks="Corona_Australis_control", n_jobs=18,
-              external_headers=False, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
+              external_headers=True, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
               name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
