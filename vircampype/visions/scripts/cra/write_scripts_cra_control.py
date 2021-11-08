82d6024f29f8c4de6a07ab9b87d8e2849f293e7b
Stefan Meingast <stefan.meingast@gmail.com>
2021-11-08 19:51:19 +0100

    Activated archiving

--- vircampype/visions/scripts/cra/write_scripts_cra_control.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_control.py
@@ -10,7 +10,7 @@ path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
 paths_files = sorted(glob(path_data + "CrA_control*/*.fits"))
 
 # Write scripts
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=False,
+write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
               projection="Corona_Australis_control", additional_source_masks="Corona_Australis_control", n_jobs=18,
               external_headers=True, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
               name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
