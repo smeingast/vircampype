02cc21395f11ba85f36391de390d0b3d7a22aa93
Stefan Meingast <stefan.meingast@gmail.com>
2021-11-09 14:02:14 +0100

    Mosaic builder for CrA epoch E

---
+++ vircampype/visions/mosaics/build_mosaic_cra_wide_e.py
@@ -0,0 +1,16 @@
+from vircampype.visions.mosaics.build_mosaic import build_mosaic
+
+# Setup for pipeline
+name = "CrA_mosaic_H_E"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_wide_E/"
+path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/CrA/"
+path_data = f"/Volumes/Data/VISIONS/198C-2009H/data_mosaic/{name}/"
+path_pype = "/Volumes/Data/VISIONS/198C-2009H/vircampype/"
+projection = "Corona_Australis_wide"
+reference_mag_lim = (12.0, 15.0)  # H wide
+phase3_photerr_internal = 0.005
+n_jobs = 18
+
+build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
+             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
+             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
