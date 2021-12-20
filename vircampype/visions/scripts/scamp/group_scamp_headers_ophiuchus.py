c37c9219321c47a2b96e0638ec486f271177f3e3
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-20 12:50:10 +0100

    Grouping Ophiuchus data (deep for now only)

---
+++ vircampype/visions/scripts/scamp/group_scamp_headers_ophiuchus.py
@@ -0,0 +1,12 @@
+import glob
+from vircampype.visions.scripts.scamp.group_scamp_headers import group_scamp_headers
+
+# Find scripts for VISIONS
+path_base = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Ophiuchus/"
+paths_scripts = sorted(glob.glob(path_base + "/**/*yml"))
+
+# Run grouping function
+path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Ophiuchus/gaia_edr3_raw_only_deep.fits"
+folder = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Ophiuchus/"
+group_scamp_headers(paths_scripts=paths_scripts, folder=folder,
+                    path_gaia_raw=path_gaia_raw, prepare_scamp=True)
