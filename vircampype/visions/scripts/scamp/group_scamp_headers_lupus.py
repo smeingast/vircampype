edf975a67192711328cc11deb3126528c8909617
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-21 14:48:13 +0100

    Scamp data splitter for Lupus (deep only for now)

---
+++ vircampype/visions/scripts/scamp/group_scamp_headers_lupus.py
@@ -0,0 +1,12 @@
+import glob
+from vircampype.visions.scripts.scamp.group_scamp_headers import group_scamp_headers
+
+# Find scripts for VISIONS
+path_base = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Lupus/"
+paths_scripts = sorted(glob.glob(path_base + "/**/*yml"))
+
+# Run grouping function
+path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Lupus/gaia_edr3_raw_only_deep.fits"
+folder = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Lupus/"
+group_scamp_headers(paths_scripts=paths_scripts, folder=folder,
+                    path_gaia_raw=path_gaia_raw, prepare_scamp=True)
