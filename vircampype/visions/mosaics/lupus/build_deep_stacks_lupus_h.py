b81da7b2a342b8c0ea93e803835735b141cb2cc5
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-23 17:21:54 +0100

    Initial scripts

---
+++ vircampype/visions/mosaics/lupus/build_deep_stacks_lupus_h.py
@@ -0,0 +1,32 @@
+import glob
+from vircampype.visions.mosaics.build_mosaic import build_mosaic
+
+# Setup for pipeline
+name = "Lupus_deep_J"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Lupus/deep/"
+path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/other/master-astro-photo/Lupus_deep/"
+path_data = f"/Volumes/Data/Mosaics/Lupus/{name}/"
+path_pype = f"/Volumes/Data/Mosaics/Lupus/vircampype/"
+reference_mag_lim = (12.0, 15.0)
+phase3_photerr_internal = 0.005
+n_jobs = 10
+
+"""
+FIRST BUILD DS9 REGION MASKS BASED ON K BAND DATA, THEN RUN ALL TILES AGAIN, THEN BUILD MOSAIC
+"""
+
+# Get script paths
+paths_scripts_n = sorted(glob.glob(f"{path_scripts}*_H_N_*.yml"))
+paths_scripts_s = sorted(glob.glob(f"{path_scripts}*_H_S_*.yml"))
+
+# Build mosaic for North field
+build_mosaic(name=name, paths_scripts=paths_scripts_n, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
+             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
+             projection="Lupus_deep_n", phase3_photerr_internal=phase3_photerr_internal,
+             build_class_star_library=False)
+
+# Build mosaic for North field
+build_mosaic(name=name, paths_scripts=paths_scripts_s, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
+             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
+             projection="Lupus_deep_s", phase3_photerr_internal=phase3_photerr_internal,
+             build_class_star_library=False)
