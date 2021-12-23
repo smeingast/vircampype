3c629dc792cdc911693497f11be60e048b06f74b
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-23 19:07:37 +0100

    Reformatted file with black

--- vircampype/visions/scripts/pipe/write_scripts_pipe_deep.py
+++ vircampype/visions/scripts/pipe/write_scripts_pipe_deep.py
@@ -12,22 +12,61 @@ paths_files_h = sorted(glob(path_data + "Pipe_deep_H*/*.fits"))
 paths_files_k = sorted(glob(path_data + "Pipe_deep_K*/*.fits"))
 
 # Write scripts for J
-write_scripts(paths_files=paths_files_j, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Pipe_deep", additional_source_masks=None, n_jobs=18,
-              external_headers=False, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_j,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Pipe_deep",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=False,
+    reference_mag_lim=(12.5, 15.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for H
-write_scripts(paths_files=paths_files_h, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Pipe_deep", additional_source_masks=None, n_jobs=18,
-              external_headers=False, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_h,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Pipe_deep",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=False,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for Ks
-write_scripts(paths_files=paths_files_k, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Pipe_deep", additional_source_masks=None, n_jobs=18,
-              external_headers=False, reference_mag_lim=(11.5, 14.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_k,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Pipe_deep",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=False,
+    reference_mag_lim=(11.5, 14.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
