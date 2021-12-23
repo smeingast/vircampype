0d587010714e51884c6e79d2b7a7c8ffd69661ad
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-23 19:08:35 +0100

    Reformatted file with black

--- vircampype/visions/mosaics/cra/build_mosaic_cra_wide_a.py
+++ vircampype/visions/mosaics/cra/build_mosaic_cra_wide_a.py
@@ -11,6 +11,15 @@ reference_mag_lim = (12.0, 15.0)  # H wide
 phase3_photerr_internal = 0.005
 n_jobs = 10
 
-build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
-             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
-             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
+build_mosaic(
+    name=name,
+    path_scripts=path_scripts,
+    path_data=path_data,
+    path_pype=path_pype,
+    n_jobs=n_jobs,
+    path_master_astro_photo=path_master_astro_photo,
+    reference_mag_lim=reference_mag_lim,
+    projection=projection,
+    phase3_photerr_internal=phase3_photerr_internal,
+    build_class_star_library=True,
+)
