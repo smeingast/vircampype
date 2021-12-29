5efc6fded741bc73525a3e6abdcfe38aeb5fd707
Stefan Meingast <stefan.meingast@gmail.com>
2021-12-29 14:16:42 +0100

    Scamp table sorting for Chamaeleon

---
+++ vircampype/visions/scripts/scamp/group_scamp_headers_chamaeleon.py
@@ -0,0 +1,17 @@
+import glob
+from vircampype.visions.scripts.scamp.group_scamp_headers import group_scamp_headers
+
+# Find scripts for VISIONS
+path_pipe = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/"
+path_scripts = f"{path_pipe}scripts/Chamaeleon/"
+paths_scripts = sorted(glob.glob(path_scripts + "/**/*yml"))
+
+# Run grouping function
+path_gaia_raw = f"{path_pipe}scamp/Chamaeleon/gaia_edr3_raw_only_deep.fits"
+folder = f"{path_pipe}scamp/Chamaeleon/"
+group_scamp_headers(
+    paths_scripts=paths_scripts,
+    folder=folder,
+    path_gaia_raw=path_gaia_raw,
+    prepare_scamp=True,
+)
