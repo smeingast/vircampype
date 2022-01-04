3d1134c7acb92719c5296b42c47772146924bb2f
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-04 17:03:09 +0100

    Simplified with kwargs

--- vircampype/visions/scripts/lupus/write_scripts_lupus_deep.py
+++ vircampype/visions/scripts/lupus/write_scripts_lupus_deep.py
@@ -14,17 +14,14 @@ paths_files_h_s = sorted(glob(path_data + "Lupus_deep_H_S*/*.fits"))
 paths_files_k_n = sorted(glob(path_data + "Lupus_deep_Ks_N*/*.fits"))
 paths_files_k_s = sorted(glob(path_data + "Lupus_deep_Ks_S*/*.fits"))
 
-# Write scripts for J North
-write_scripts(
-    paths_files=paths_files_j_n,
+# Common kwargs
+kwargs = dict(
     path_pype=path_pype,
     path_scripts=path_scripts,
     archive=False,
-    projection="Lupus_deep_n",
-    additional_source_masks=None,
+    additional_source_masks="Lupus_deep",
     n_jobs=18,
     external_headers=True,
-    reference_mag_lim=(12.5, 15.5),
     phase3_photerr_internal=0.005,
     name_suffix=None,
     build_stacks=False,
@@ -32,104 +29,52 @@ write_scripts(
     build_phase3=False,
     build_class_star_library=False,
     name_from_directory=False,
+)
+
+# Write scripts for J North
+write_scripts(
+    paths_files=paths_files_j_n,
+    projection="Lupus_deep_n",
+    reference_mag_lim=(12.5, 15.5),
+    **kwargs
 )
 
 # Write scripts for J South
 write_scripts(
     paths_files=paths_files_j_s,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
     projection="Lupus_deep_s",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
     reference_mag_lim=(12.5, 15.5),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
+    **kwargs
 )
 
 # Write scripts for H North
 write_scripts(
     paths_files=paths_files_h_n,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
     projection="Lupus_deep_n",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
     reference_mag_lim=(12.0, 15.0),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
+    **kwargs
 )
 
 # Write scripts for H South
 write_scripts(
     paths_files=paths_files_h_s,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
     projection="Lupus_deep_s",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
     reference_mag_lim=(12.0, 15.0),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
+    **kwargs
 )
 
 # Write scripts for Ks North
 write_scripts(
     paths_files=paths_files_k_n,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
     projection="Lupus_deep_n",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
     reference_mag_lim=(11.5, 14.5),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
+    **kwargs
 )
 
 # Write scripts for Ks South
 write_scripts(
     paths_files=paths_files_k_s,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
     projection="Lupus_deep_s",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
     reference_mag_lim=(11.5, 14.5),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
+    **kwargs
 )
