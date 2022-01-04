d17f362de8f70884814d7b09d10757f44f2d40bf
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-04 16:08:28 +0100

    Formatted file with black

--- vircampype/visions/scripts/cra/write_scripts_cra_deep.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_deep.py
@@ -8,21 +8,57 @@ path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
 
 # Write scripts for J
 paths_files = sorted(glob(path_data + "CrA_deep_J*/*.fits"))
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_deep", additional_source_masks="Corona_Australis_deep", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
+write_scripts(
+    paths_files=paths_files,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=True,
+    projection="Corona_Australis_deep",
+    additional_source_masks="Corona_Australis_deep",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.5, 15.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+)
 
 # Write scripts for H
 paths_files = sorted(glob(path_data + "CrA_deep_H*/*.fits"))
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_deep", additional_source_masks="Corona_Australis_deep", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
+write_scripts(
+    paths_files=paths_files,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=True,
+    projection="Corona_Australis_deep",
+    additional_source_masks="Corona_Australis_deep",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+)
 
 # Write scripts for Ks
 paths_files = sorted(glob(path_data + "CrA_deep_Ks*/*.fits"))
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_deep", additional_source_masks="Corona_Australis_deep", n_jobs=18,
-              external_headers=True, reference_mag_lim=(11.5, 14.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=True, build_tile=True, build_phase3=True)
+write_scripts(
+    paths_files=paths_files,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=True,
+    projection="Corona_Australis_deep",
+    additional_source_masks="Corona_Australis_deep",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(11.5, 14.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+)
