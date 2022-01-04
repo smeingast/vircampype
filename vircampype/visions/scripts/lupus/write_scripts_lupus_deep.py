a7c00177886729a25595468d1b2f5e1319c96ac9
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-04 16:55:11 +0100

    Formatted file with black

--- vircampype/visions/scripts/lupus/write_scripts_lupus_deep.py
+++ vircampype/visions/scripts/lupus/write_scripts_lupus_deep.py
@@ -15,43 +15,121 @@ paths_files_k_n = sorted(glob(path_data + "Lupus_deep_Ks_N*/*.fits"))
 paths_files_k_s = sorted(glob(path_data + "Lupus_deep_Ks_S*/*.fits"))
 
 # Write scripts for J North
-write_scripts(paths_files=paths_files_j_n, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_n", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_j_n,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_n",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.5, 15.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for J South
-write_scripts(paths_files=paths_files_j_s, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_s", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.5, 15.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_j_s,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_s",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.5, 15.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for H North
-write_scripts(paths_files=paths_files_h_n, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_n", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_h_n,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_n",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for H South
-write_scripts(paths_files=paths_files_h_s, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_s", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_h_s,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_s",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for Ks North
-write_scripts(paths_files=paths_files_k_n, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_n", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(11.5, 14.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_k_n,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_n",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(11.5, 14.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
 
 # Write scripts for Ks South
-write_scripts(paths_files=paths_files_k_s, path_pype=path_pype, path_scripts=path_scripts, archive=False,
-              projection="Lupus_deep_s", additional_source_masks=None, n_jobs=18,
-              external_headers=True, reference_mag_lim=(11.5, 14.5), phase3_photerr_internal=0.005,
-              name_suffix=None, build_stacks=False, build_tile=True, build_phase3=False,
-              build_class_star_library=False, name_from_directory=False)
+write_scripts(
+    paths_files=paths_files_k_s,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=False,
+    projection="Lupus_deep_s",
+    additional_source_masks=None,
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(11.5, 14.5),
+    phase3_photerr_internal=0.005,
+    name_suffix=None,
+    build_stacks=False,
+    build_tile=True,
+    build_phase3=False,
+    build_class_star_library=False,
+    name_from_directory=False,
+)
