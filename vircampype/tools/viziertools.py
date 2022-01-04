da00b1037d7b3c20002678d4af08335e7fca275b
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-04 15:33:50 +0100

    Formatted file with black

--- vircampype/tools/viziertools.py
+++ vircampype/tools/viziertools.py
@@ -20,10 +20,16 @@ def download_2mass(skycoord, radius):
     """
 
     # Setup for Vizier
-    v = Vizier(columns=["*", "errMaj", "errMin", "errPA", "JD"], catalog="II/246/out", row_limit=-1)
+    v = Vizier(
+        columns=["*", "errMaj", "errMin", "errPA", "JD"],
+        catalog="II/246/out",
+        row_limit=-1,
+    )
 
     # Submit query
-    result = v.query_region(skycoord, radius=radius * Unit("deg"), catalog="II/246/out")[0]
+    result = v.query_region(
+        skycoord, radius=radius * Unit("deg"), catalog="II/246/out"
+    )[0]
     del result.meta["description"]
     result.meta["NAME"] = "2MASS"
 
@@ -53,7 +59,9 @@ def download_gaia(skycoord, radius):
     v = Vizier(columns=["*"], catalog="I/350/gaiaedr3", row_limit=-1)
 
     # Submit query
-    result = v.query_region(skycoord, radius=radius * Unit("deg"), catalog="I/350/gaiaedr3")[0]
+    result = v.query_region(
+        skycoord, radius=radius * Unit("deg"), catalog="I/350/gaiaedr3"
+    )[0]
     del result.meta["description"]
     result.meta["NAME"] = "Gaia EDR3"
 
