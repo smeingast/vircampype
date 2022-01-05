35bd447764f9dfc91251a2004223cef0ffe77a50
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-05 12:40:34 +0100

    Reformatted and updated script writer with masks

--- vircampype/visions/scripts/chamaeleon/write_scripts_chamaeleon_deep.py
+++ vircampype/visions/scripts/chamaeleon/write_scripts_chamaeleon_deep.py
@@ -13,17 +13,15 @@ paths_files_j = sorted(glob(path_data + "Chamaeleon_deep_J*/*.fits"))
 paths_files_h = sorted(glob(path_data + "Chamaeleon_deep_H*/*.fits"))
 paths_files_k = sorted(glob(path_data + "Chamaeleon_deep_K*/*.fits"))
 
-# Write scripts for J
-write_scripts(
-    paths_files=paths_files_j,
+# Common kwargs
+kwargs = dict(
     path_pype=path_pype,
     path_scripts=path_scripts,
     archive=False,
     projection="Chamaeleon_deep",
-    additional_source_masks=None,
+    additional_source_masks="Chamaeleon_deep",
     n_jobs=18,
     external_headers=True,
-    reference_mag_lim=(12.5, 15.5),
     phase3_photerr_internal=0.005,
     name_suffix=None,
     build_stacks=False,
@@ -32,43 +30,12 @@ write_scripts(
     build_class_star_library=False,
     name_from_directory=False,
 )
+
+# Write scripts for J
+write_scripts(paths_files=paths_files_j, reference_mag_lim=(12.5, 15.5), **kwargs)
 
 # Write scripts for H
-write_scripts(
-    paths_files=paths_files_h,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
-    projection="Chamaeleon_deep",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
-    reference_mag_lim=(12.0, 15.0),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
-)
+write_scripts(paths_files=paths_files_h, reference_mag_lim=(12.0, 15.0), **kwargs)
 
 # Write scripts for Ks
-write_scripts(
-    paths_files=paths_files_k,
-    path_pype=path_pype,
-    path_scripts=path_scripts,
-    archive=False,
-    projection="Chamaeleon_deep",
-    additional_source_masks=None,
-    n_jobs=18,
-    external_headers=True,
-    reference_mag_lim=(11.5, 14.5),
-    phase3_photerr_internal=0.005,
-    name_suffix=None,
-    build_stacks=False,
-    build_tile=True,
-    build_phase3=False,
-    build_class_star_library=False,
-    name_from_directory=False,
-)
+write_scripts(paths_files=paths_files_k, reference_mag_lim=(11.5, 14.5), **kwargs)
