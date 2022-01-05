f3748ce5b30b5d63d49474eefadd6d3e4c0ee8b0
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-05 13:28:48 +0100

    Reformatted file

--- vircampype/visions/scamp/group_scamp_headers_lupus.py
+++ vircampype/visions/scamp/group_scamp_headers_lupus.py
@@ -6,7 +6,11 @@ path_base = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Lupus/"
 paths_scripts = sorted(glob.glob(path_base + "/**/*yml"))
 
 # Run grouping function
-path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Lupus/gaia_edr3_raw_only_deep.fits"
-folder = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Lupus/"
-group_scamp_headers(paths_scripts=paths_scripts, folder=folder,
-                    path_gaia_raw=path_gaia_raw, prepare_scamp=True)
+path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/" \
+                "scamp/Lupus/gaia_edr3_raw_only_deep.fits"
+group_scamp_headers(
+    paths_scripts=paths_scripts,
+    path_gaia_raw=path_gaia_raw,
+    folder="/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Lupus/",
+    prepare_scamp=True,
+)
