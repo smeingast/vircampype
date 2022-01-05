428157703dd4a98abc9741ea27c05e3d587bcb81
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-05 21:23:24 +0100

    Reformatted file

--- vircampype/visions/scamp/group_scamp_headers_ophiuchus.py
+++ vircampype/visions/scamp/group_scamp_headers_ophiuchus.py
@@ -6,7 +6,12 @@ path_base = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Ophiuchus/"
 paths_scripts = sorted(glob.glob(path_base + "/**/*yml"))
 
 # Run grouping function
-path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Ophiuchus/gaia_edr3_raw_only_deep.fits"
+path_gaia_raw = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/" \
+                "scamp/Ophiuchus/gaia_edr3_raw_only_deep.fits"
 folder = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scamp/Ophiuchus/"
-group_scamp_headers(paths_scripts=paths_scripts, folder=folder,
-                    path_gaia_raw=path_gaia_raw, prepare_scamp=True)
+group_scamp_headers(
+    paths_scripts=paths_scripts,
+    path_gaia_raw=path_gaia_raw,
+    folder=folder,
+    prepare_scamp=True,
+)
