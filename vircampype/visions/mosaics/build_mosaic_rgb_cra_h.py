ccd405350653285ef3de3be1ec783e9ef377faf9
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-05 16:08:16 +0100

    Reformatted file

--- vircampype/visions/mosaics/build_mosaic_rgb_cra_h.py
+++ vircampype/visions/mosaics/build_mosaic_rgb_cra_h.py
@@ -3,8 +3,17 @@ from vircampype.visions.mosaics.build_mosaic_rgb import build_mosaic_rgb
 
 name_mosaic = "CrA_mosaic_RGB_H"
 path_mosaic = "/Volumes/Data/Mosaics/CrA/RGB_H/"
-paths_tiles_wide = sorted(glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_wide*_C/tile/*C.fits"))
-paths_tiles_deep = sorted(glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_deep_H_*/tile/*[1-2].fits"))
+paths_tiles_wide = sorted(
+    glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_wide*_C/tile/*C.fits")
+)
+paths_tiles_deep = sorted(
+    glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_deep_H_*/tile/*[1-2].fits")
+)
 
-build_mosaic_rgb(paths_tiles_wide=paths_tiles_wide, paths_tiles_deep=paths_tiles_deep, path_mosaic=path_mosaic,
-                 name_mosaic=name_mosaic, field_name="Corona_Australis_wide")
+build_mosaic_rgb(
+    paths_tiles_wide=paths_tiles_wide,
+    paths_tiles_deep=paths_tiles_deep,
+    path_mosaic=path_mosaic,
+    name_mosaic=name_mosaic,
+    field_name="Corona_Australis_wide",
+)
