268170112c2e2e32609fa230ce06664b93ee9929
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-07 09:23:44 +0100

    Updated scripts path

--- vircampype/visions/scripts/cra/write_scripts_cra_wide_b.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_wide_b.py
@@ -4,16 +4,31 @@ from vircampype.visions.scripts.write_scripts import write_scripts
 # Define paths
 path_data = "/Volumes/Data/VISIONS/198C-2009A/data_wide/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009A/vircampype/"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_wide_B/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/wide_B/"
 
 # Search for files
 paths_files = sorted(glob(path_data + "CrA*/B/*.fits"))
 
+# Common kwargs
+kwargs = dict(
+    archive=True,
+    projection="Corona_Australis_wide",
+    additional_source_masks="Corona_Australis_wide",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix="_B",
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+    build_class_star_library=True,
+)
+
 # Write scripts
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix="_B", build_stacks=True, build_tile=True, build_phase3=True, build_class_star_library=True)
+write_scripts(
+    paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, **kwargs
+)
 
 # Define paths again for compensation run
 path_data = "/Volumes/Data/VISIONS/198C-2009B/data_wide/"
@@ -23,8 +38,6 @@ path_pype = "/Volumes/Data/VISIONS/198C-2009B/vircampype/"
 paths_files = sorted(glob(path_data + "CrA*/B/*.fits"))
 
 # Write scripts
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix="_B", build_stacks=True, build_tile=True, build_phase3=True, build_class_star_library=True)
-
+write_scripts(
+    paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, **kwargs
+)
