c4e6c913bcfda1776e165ebda9c3402d962df4ea
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-07 09:00:11 +0100

    Reformatted files

--- vircampype/visions/mosaics/ophiuchus/build_deep_stack_oph_h.py
+++ vircampype/visions/mosaics/ophiuchus/build_deep_stack_oph_h.py
@@ -3,8 +3,9 @@ from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "Ophiuchus_deep_H"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/Ophiuchus/deep/"
-path_master_astro_photo = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/other/master-astro-photo/Ophiuchus_deep/"
+path_pipe = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/"
+path_scripts = f"{path_pipe}scripts/Ophiuchus/deep/"
+path_master_astro_photo = f"{path_pipe}other/master-astro-photo/Ophiuchus_deep/"
 path_data = f"/Volumes/Data/Mosaics/Ophiuchus/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/Ophiuchus/vircampype/"
 projection = "Ophiuchus_deep"
@@ -16,6 +17,16 @@ n_jobs = 10
 paths_scripts = sorted(glob.glob(f"{path_scripts}*_H_*.yml"))
 
 # Build mosaic
-build_mosaic(name=name, paths_scripts=paths_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
-             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
-             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=False)
+build_mosaic(
+    name=name,
+    paths_scripts=paths_scripts,
+    path_data=path_data,
+    path_pype=path_pype,
+    n_jobs=n_jobs,
+    path_master_astro_photo=path_master_astro_photo,
+    reference_mag_lim=reference_mag_lim,
+    projection=projection,
+    phase3_photerr_internal=phase3_photerr_internal,
+    build_class_star_library=False,
+    build_phase3=False,
+)
