6bf97f83f1bb5cfea287b1f64d71c92ebf83d819
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-07 09:10:57 +0100

    Reformatted file

--- vircampype/visions/headers_scripts/build_field_header_corona_australis_deep.py
+++ vircampype/visions/headers_scripts/build_field_header_corona_australis_deep.py
@@ -7,10 +7,12 @@ path_base = "/Volumes/Data/VISIONS/198C-2009E/data_deep/"
 files = glob.glob(path_base + "CrA*/*.fits")
 
 # Set dummy pipeline setup
-setup = dict(name="CrA_deep",
-             path_data="/dev/null",
-             path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
-             n_jobs=6)
+setup = dict(
+    name="CrA_deep",
+    path_data="/dev/null",
+    path_pype="/Users/stefan/Dropbox/Data/VISIONS/test/vircampype/",
+    n_jobs=6,
+)
 
 # Instantiate files
 images = SkyImagesRawScience(setup=setup, file_paths=files)
