47e5263b4cca550dd762d6626cc93301b86ce08f
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-09 13:06:24 +0100

    Updated scripts path

--- vircampype/visions/scripts/cra/write_scripts_cra_wide_e.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_wide_e.py
@@ -2,7 +2,7 @@ from glob import glob
 from vircampype.visions.scripts.write_scripts import write_scripts
 
 # Define paths
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_wide_E/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/wide_E/"
 path_data = "/Volumes/Data/VISIONS/198C-2009H/data_wide/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009H/vircampype/"
 
@@ -10,10 +10,23 @@ path_pype = "/Volumes/Data/VISIONS/198C-2009H/vircampype/"
 paths_files = sorted(glob(path_data + "CrA*/A/*.fits"))
 
 # Write scripts
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix="_E", build_stacks=True, build_tile=True, build_phase3=True, build_class_star_library=True)
+write_scripts(
+    paths_files=paths_files,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=True,
+    projection="Corona_Australis_wide",
+    additional_source_masks="Corona_Australis_wide",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix="_E",
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+    build_class_star_library=True,
+)
 
 """ Four tiles are missing from this run. These were done later in run L. """
 
@@ -25,8 +38,20 @@ path_pype = "/Volumes/Data/VISIONS/198C-2009L/vircampype/"
 paths_files = sorted(glob(path_data + "CrA*/*.fits"))
 
 # Write scripts
-write_scripts(paths_files=paths_files, path_pype=path_pype, path_scripts=path_scripts, archive=True,
-              projection="Corona_Australis_wide", additional_source_masks="Corona_Australis_wide", n_jobs=18,
-              external_headers=True, reference_mag_lim=(12.0, 15.0), phase3_photerr_internal=0.005,
-              name_suffix="_E", build_stacks=True, build_tile=True, build_phase3=True, build_class_star_library=True)
-
+write_scripts(
+    paths_files=paths_files,
+    path_pype=path_pype,
+    path_scripts=path_scripts,
+    archive=True,
+    projection="Corona_Australis_wide",
+    additional_source_masks="Corona_Australis_wide",
+    n_jobs=18,
+    external_headers=True,
+    reference_mag_lim=(12.0, 15.0),
+    phase3_photerr_internal=0.005,
+    name_suffix="_E",
+    build_stacks=True,
+    build_tile=True,
+    build_phase3=True,
+    build_class_star_library=True,
+)
