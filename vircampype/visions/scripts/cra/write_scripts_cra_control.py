2c509edaa38dba935a9f2a70022e5879920b1917
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-10 14:27:49 +0100

    Fixed saturation levels for different passbands

--- vircampype/visions/scripts/cra/write_scripts_cra_control.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_control.py
@@ -7,11 +7,12 @@ path_data = "/Volumes/Data/VISIONS/198C-2009E/data_control/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
 
 # Search for files
-paths_files = sorted(glob(path_data + "CrA_control*/*.fits"))
+paths_files_j = sorted(glob(path_data + "CrA_control_J/*.fits"))
+paths_files_h = sorted(glob(path_data + "CrA_control_H/*.fits"))
+paths_files_ks = sorted(glob(path_data + "CrA_control_Ks/*.fits"))
 
-# Write scripts
-write_scripts(
-    paths_files=paths_files,
+# Common kwargs
+kwargs = dict(
     path_pype=path_pype,
     path_scripts=path_scripts,
     archive=True,
@@ -19,10 +20,14 @@ write_scripts(
     additional_source_masks="Corona_Australis_control",
     n_jobs=18,
     external_headers=True,
-    reference_mag_lim=(12.5, 15.5),
     phase3_photerr_internal=0.005,
     name_suffix=None,
     build_stacks=True,
     build_tile=True,
     build_phase3=True,
 )
+
+# Write scripts
+write_scripts(paths_files=paths_files_j, reference_mag_lim=(12.5, 15.5), **kwargs)
+write_scripts(paths_files=paths_files_h, reference_mag_lim=(12.0, 15.0), **kwargs)
+write_scripts(paths_files=paths_files_ks, reference_mag_lim=(11.5, 14.5), **kwargs)
