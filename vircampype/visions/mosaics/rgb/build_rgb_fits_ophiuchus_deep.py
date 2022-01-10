485263cbb0aff8e2cdaf40d11ece5ab5ba1ccc38
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-10 10:12:56 +0100

    jpg to fits conversion for Ophiuchus deep

---
+++ vircampype/visions/mosaics/rgb/build_rgb_fits_ophiuchus_deep.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+from vircampype.visions.mosaics.rgb.utils import jpg2fits
+
+# File paths
+path_base = Path("/Users/stefan/Dropbox/Projects/VISIONS/RGB/Ophiuchus")
+path_jpg = path_base / "Ophiuchus_deep.jpg"
+path_fits = path_base / "Ophiuchus_deep_J.fits"
+
+# Weight paths
+paths_weights = path_base.glob("*.weight.fits")
+
+# Convert channels to fits
+jpg2fits(path_jpg=path_jpg, path_fits=path_fits, paths_weights=paths_weights)
