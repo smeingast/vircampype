653594fab3ea435b44dea158f7f3d3beecc1c2dc
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-11 15:42:02 +0100

    Reformatted and updated paths

--- vircampype/visions/mosaics/cra/build_deep_stack_cra_j.py
+++ vircampype/visions/mosaics/cra/build_deep_stack_cra_j.py
@@ -1,16 +1,32 @@
+import glob
 from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "CrA_deep_J"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_deep_J/"
-path_master_astro_photo = "/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_deep_J_1/master/"
+path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/deep/"
+path_master_astro_photo = (
+    "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/other/master-astro-photo/CrA_deep/"
+)
 path_data = f"/Volumes/Data/Mosaics/CrA/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/CrA/vircampype/"
-projection = "Corona_Australis_deep"
 reference_mag_lim = (12.5, 15.5)
 phase3_photerr_internal = 0.005
 n_jobs = 10
 
-build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
-             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
-             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
+# Get script paths
+paths_scripts = sorted(glob.glob(f"{path_scripts}*_J_*.yml"))
+
+# Build mosaic
+build_mosaic(
+    name=name,
+    paths_scripts=paths_scripts,
+    path_data=path_data,
+    path_pype=path_pype,
+    n_jobs=n_jobs,
+    path_master_astro_photo=path_master_astro_photo,
+    reference_mag_lim=reference_mag_lim,
+    projection="Corona_Australis_deep",
+    phase3_photerr_internal=phase3_photerr_internal,
+    build_class_star_library=False,
+    build_phase3=False,
+)
