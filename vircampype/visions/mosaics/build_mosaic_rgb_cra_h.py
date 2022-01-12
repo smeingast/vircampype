be258168fc4d73510ecdff2297dcb083bb9adc33
Stefan Meingast <stefan.meingast@gmail.com>
2022-01-12 15:32:07 +0100

    Updated paths and names

--- vircampype/visions/mosaics/build_mosaic_rgb_cra_h.py
+++ vircampype/visions/mosaics/build_mosaic_rgb_cra_h.py
@@ -1,18 +1,16 @@
 from glob import glob
 from vircampype.visions.mosaics.build_mosaic_rgb import build_mosaic_rgb
 
-name_mosaic = "CrA_mosaic_RGB_H"
+name_mosaic = "CrA_RGB_H"
 path_mosaic = "/Volumes/Data/Mosaics/CrA/RGB_H/"
 paths_tiles_wide = sorted(
-    glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_wide*_C/tile/*C.fits")
+    glob("/Volumes/Data/VISIONS/198C-2009A/vircampype/CrA_wide*_A/tile/*A.fits")
 )
-paths_tiles_deep = sorted(
-    glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_deep_H_*/tile/*[1-2].fits")
-)
+path_deep_stack = "/Volumes/Data/Mosaics/CrA/vircampype/CrA_deep_H/tile/CrA_deep_H.fits"
 
 build_mosaic_rgb(
     paths_tiles_wide=paths_tiles_wide,
-    paths_tiles_deep=paths_tiles_deep,
+    path_deep_stack=path_deep_stack,
     path_mosaic=path_mosaic,
     name_mosaic=name_mosaic,
     field_name="Corona_Australis_wide",
--- vircampype/visions/mosaics/build_mosaic_rgb_cra_ks.py
+++ vircampype/visions/mosaics/build_mosaic_rgb_cra_ks.py
@@ -1,18 +1,18 @@
 from glob import glob
 from vircampype.visions.mosaics.build_mosaic_rgb import build_mosaic_rgb
 
-name_mosaic = "CrA_mosaic_RGB_Ks"
+name_mosaic = "CrA_RGB_Ks"
 path_mosaic = "/Volumes/Data/Mosaics/CrA/RGB_Ks/"
 paths_tiles_wide = sorted(
     glob("/Volumes/Data/VHS/CrA/vircampype/*GPS*_Ks/tile/*Ks.fits")
 )
-paths_tiles_deep = sorted(
-    glob("/Volumes/Data/VISIONS/198C-2009E/vircampype/CrA_deep_Ks_*/tile/*[1-2].fits")
+path_deep_stack = (
+    "/Volumes/Data/Mosaics/CrA/vircampype/CrA_deep_Ks/tile/CrA_deep_ks.fits"
 )
 
 build_mosaic_rgb(
     paths_tiles_wide=paths_tiles_wide,
-    paths_tiles_deep=paths_tiles_deep,
+    path_deep_stack=path_deep_stack,
     path_mosaic=path_mosaic,
     name_mosaic=name_mosaic,
     field_name="Corona_Australis_wide",
