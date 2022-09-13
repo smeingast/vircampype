43dfe54d0e9ce2bb7a5202b2583226c98e08d251
Stefan Meingast <stefan.meingast@gmail.com>
2022-09-13 11:49:42 +0200

    Reformatted with black

--- setup.py
+++ setup.py
@@ -10,12 +10,22 @@ if sys.version_info < (3, 7):
 setup(
     name="vircampype",
     version=__version__,
-    install_requires=["numpy>=1.17", "scipy>=1.3", "matplotlib>=3.1", "astropy>=3.1", "astroquery>=0.3.9",
-                      "pyyaml>=3", "joblib>0.12", "scikit-learn>=0.23", "pillow>=6.1", "scikit-image>0.18"],
+    install_requires=[
+        "numpy>=1.17",
+        "scipy>=1.3",
+        "matplotlib>=3.1",
+        "astropy>=3.1",
+        "astroquery>=0.3.9",
+        "pyyaml>=3",
+        "joblib>0.12",
+        "scikit-learn>=0.23",
+        "pillow>=6.1",
+        "scikit-image>0.18",
+    ],
     packages=["vircampype"],
     url="",
     license="",
     author="Stefan Meingast",
     author_email="stefan.meingast@univie.ac.at",
-    description="Pipeline for processing VIRCAM data."
+    description="Pipeline for processing VIRCAM data.",
 )
