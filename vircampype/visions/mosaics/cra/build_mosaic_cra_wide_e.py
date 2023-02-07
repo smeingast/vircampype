35dce8e1e05b478a817b4392e8cb02a8c4d111be
Stefan Meingast <stefan.meingast@gmail.com>
2023-02-07 11:01:59 +0100

    Adapted script for new mosaic method

--- vircampype/visions/mosaics/cra/build_mosaic_cra_wide_e.py
+++ vircampype/visions/mosaics/cra/build_mosaic_cra_wide_e.py
@@ -1,16 +1,30 @@
+import glob
 from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "CrA_mosaic_H_E"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/scripts/VISIONS/CrA/CrA_wide_E/"
-path_master_astro_photo = "/Users/stefan/Dropbox/Data/VISIONS/Master/CrA/Mosaic/"
+path_visions = "/Users/stefan/Dropbox/Projects/VISIONS/"
+path_scripts = f"{path_visions}Pipeline/scripts/CrA/wide_E/"
+path_master_astro_photo = f"{path_visions}Pipeline/other/master-astro-photo/CrA_wide/"
 path_data = f"/Volumes/Data/Mosaics/CrA/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/CrA/vircampype/"
 projection = "Corona_Australis_wide"
 reference_mag_lim = (12.0, 15.0)  # H wide
-phase3_photerr_internal = 0.005
+photerr_internal = 0.005
 n_jobs = 10
 
-build_mosaic(name=name, path_scripts=path_scripts, path_data=path_data, path_pype=path_pype, n_jobs=n_jobs,
-             path_master_astro_photo=path_master_astro_photo, reference_mag_lim=reference_mag_lim,
-             projection=projection, phase3_photerr_internal=phase3_photerr_internal, build_class_star_library=True)
+# Get script paths
+paths_scripts = sorted(glob.glob(f"{path_scripts}*.yml"))
+
+build_mosaic(
+    name=name,
+    paths_scripts=paths_scripts,
+    path_data=path_data,
+    path_pype=path_pype,
+    n_jobs=n_jobs,
+    path_master_astro_photo=path_master_astro_photo,
+    reference_mag_lim=reference_mag_lim,
+    projection=projection,
+    photerr_internal=photerr_internal,
+    build_public_catalog=True,
+)
