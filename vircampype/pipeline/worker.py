de78ff2d9ba1e170e867901349a2f5c07a36383b
Stefan Meingast <stefan.meingast@gmail.com>
2023-03-28 23:29:24 +0200

    Implemented new sorting functions and added sort argument to worker

--- vircampype/pipeline/worker.py
+++ vircampype/pipeline/worker.py
@@ -5,22 +5,31 @@ export PYTHONPATH="/Users/stefan/Dropbox/Projects/vircampype/":$PYTHONPATH}
 """
 import sys
 import argparse
+from vircampype.tools.datatools import *
 from vircampype.pipeline.main import Pipeline
 
 
 def main():
     # Setup parser
     parser = argparse.ArgumentParser(description="Pipeline for VIRCAM images.")
+    parser.add_argument("-s", "--setup", help="Input setup file", type=list)
     parser.add_argument(
-        "-s",
-        "--setup",
-        help="Input setup file",
-        type=str,
-        required=True
+        "--sort",
+        help="Sort files by passing their paths (e.g. /path/to/files/*fits)",
+        nargs="+",
+        default=None,
     )
 
     # Parse arguments
     args = parser.parse_args()
+
+    if isinstance(args.sort, list):
+        paths_science, paths_calib = split_in_science_and_calibration(
+            paths_files=args.sort
+        )
+        sort_vircam_calibration(paths_calib=paths_calib)
+        sort_vircam_science(paths_science=paths_science)
+        return
 
     # Initialize pipeline
     pipeline = Pipeline(setup=args.setup)
--- vircampype/tools/datatools.py
+++ vircampype/tools/datatools.py
@@ -1,94 +1,102 @@
 import os
-import glob
 import shutil
 
 from astropy.io import fits
+from typing import List, Optional
 from vircampype.tools.systemtools import make_folder
 
-__all__ = ["sort_vircam_calibration", "sort_vircam_science", "sort_by_passband"]
+__all__ = [
+    "split_in_science_and_calibration",
+    "sort_vircam_calibration",
+    "sort_vircam_science",
+    "sort_by_passband",
+]
 
 
-def sort_vircam_calibration(path_all, path_calibration, extension=".fits"):
-
-    # Add '/' if necessary
-    if not path_all.endswith("/"):
-        path_all += "/"
-    if not path_calibration.endswith("/"):
-        path_calibration += "/"
-
-    # Make calibration folder
-    make_folder(path=path_calibration)
-
-    # Find files
-    paths_all = glob.glob(pathname="{0}*{1}".format(path_all, extension))
-
-    if len(paths_all) == 0:
+def split_in_science_and_calibration(paths_files: List) -> (List, List):
+    # Check that at least one file is there
+    if len(paths_files) == 0:
         raise ValueError("No files found!")
 
-    # Get category
-    catg_all = [fits.getheader(filename=f)["HIERARCH ESO DPR CATG"] for f in paths_all]
+    # Get absolute filepaths
+    paths_files = [os.path.abspath(p) for p in paths_files]
 
-    idx_calib = [i for i, j in enumerate(catg_all) if j == "CALIB"]
+    # Get category
+    catg_all = [
+        fits.getheader(filename=f)["HIERARCH ESO DPR CATG"] for f in paths_files
+    ]
+
+    # Grab science and calibration file indices
     idx_science = [i for i, j in enumerate(catg_all) if j == "SCIENCE"]
+    idx_calib = [i for i, j in enumerate(catg_all) if j == "CALIB"]
 
     # Dummy check
-    if len(idx_calib) + len(idx_science) != len(paths_all):
+    if len(idx_calib) + len(idx_science) != len(paths_files):
         raise ValueError("Input and output not matching")
 
     # Get paths for calibration files
-    paths_calib = [paths_all[i] for i in idx_calib]
+    paths_science = [paths_files[i] for i in idx_science]
+    paths_calib = [paths_files[i] for i in idx_calib]
+
+    # Return
+    return paths_science, paths_calib
+
+
+def sort_vircam_calibration(paths_calib: List) -> Optional[List]:
+    # Check that at least one file is there
+    if len(paths_calib) == 0:
+        return
+
+    # Get absolute filepaths
+    paths_calib = [os.path.abspath(p) for p in paths_calib]
+
+    # Get current working directory and make calibration folder
+    path_folder = f"{os.getcwd()}/Calibration/"
+    make_folder(path=path_folder)
 
     # Move files to calibration directory
     for p in paths_calib:
+        shutil.move(p, path_folder)
 
-        # If file exists, remove and continue
-        if os.path.exists(path_calibration + os.path.basename(p)):
-            os.remove(p)
-
-        # Otherwise move
-        else:
-            shutil.move(p, path_calibration)
+    return [f"{path_folder}{p}" for p in paths_calib]
 
 
-def sort_vircam_science(path, extension=".fits"):
+def sort_vircam_science(paths_science: List) -> Optional[List]:
+    # Check that files are actually provide
+    if len(paths_science) == 0:
+        return
 
-    # Add '/' if necessary
-    if not path.endswith("/"):
-        path += "/"
+    # Get absolute filepaths
+    paths_science = [os.path.abspath(p) for p in paths_science]
 
-    # Find files
-    paths_orig = glob.glob(pathname="{0}*{1}".format(path, extension))
-    file_names = [os.path.basename(f) for f in paths_orig]
-    paths_dirs = ["{0}/".format(os.path.dirname(f)) for f in paths_orig]
-
-    if len(paths_orig) == 0:
-        raise ValueError("No files found!")
+    # Get directories and filenames
+    paths_dirs = [f"{os.path.dirname(f)}/" for f in paths_science]
+    file_names = [os.path.basename(f) for f in paths_science]
 
     # Get Object Name
-    obj = [
-        fits.getheader(filename=f)["HIERARCH ESO OBS NAME"].replace("VISIONS_", "")
-        for f in paths_orig
-    ]
+    obj = [fits.getheader(filename=f)["HIERARCH ESO OBS NAME"] for f in paths_science]
 
     # Identify unique objects
     uobj = sorted(list(set(obj)))
 
+    # Get current working directory
+    cwd = f"{os.getcwd()}/"
+
     # Make folders
     for uo in uobj:
-        make_folder(path=path + uo)
+        make_folder(path=f"{cwd}{uo}")
 
     # Construct output paths
-    paths_move = [
-        "{0}{1}/{2}".format(d, o, f) for d, o, f in zip(paths_dirs, obj, file_names)
-    ]
+    paths_move = [f"{d}{o}/{f}" for d, o, f in zip(paths_dirs, obj, file_names)]
 
     # Move files to folders
-    for po, pm in zip(paths_orig, paths_move):
+    for po, pm in zip(paths_science, paths_move):
         shutil.move(po, pm)
+
+    return paths_move
 
 
 def sort_by_passband(paths):
-
     # Get base directories of files
     directories = [f"{os.path.dirname(p)}/" for p in paths]
 
@@ -99,7 +107,6 @@ def sort_by_passband(paths):
 
     # Loop over files and sort into subdirectories
     for pp, dd, pb in zip(paths, directories, passbands):
-
         # Make directories
         make_folder(f"{dd}{pb}")
 
