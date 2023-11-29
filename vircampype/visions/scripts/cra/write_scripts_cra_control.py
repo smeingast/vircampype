e9a429d3b46c78ccfac35fd5ed1743ecaacfe27e
Stefan Meingast <stefan.meingast@gmail.com>
2023-11-29 10:29:53 +0100

    Updated to use new keywords

--- vircampype/visions/scripts/cra/write_scripts_cra_control.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_control.py
@@ -2,7 +2,7 @@ from glob import glob
 from vircampype.visions.scripts.write_scripts import write_scripts
 
 # Define paths
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/control/"
+path_scripts = "/Users/stefan/MyCloud/Projects/VISIONS/Pipeline/scripts/CrA/control/"
 path_data = "/Volumes/Data/VISIONS/198C-2009E/data_control/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009E/vircampype/"
 
@@ -20,7 +20,7 @@ kwargs = dict(
     additional_source_masks="Corona_Australis_control",
     n_jobs=18,
     external_headers=True,
-    phase3_photerr_internal=0.005,
+    photometric_error_floor=0.005,
     name_suffix=None,
     build_stacks=True,
     build_tile=True,
@@ -29,6 +29,12 @@ kwargs = dict(
 )
 
 # Write scripts
-write_scripts(paths_files=paths_files_j, reference_mag_lim=(12.5, 15.5), **kwargs)
-write_scripts(paths_files=paths_files_h, reference_mag_lim=(12.0, 15.0), **kwargs)
-write_scripts(paths_files=paths_files_ks, reference_mag_lim=(11.5, 14.5), **kwargs)
+write_scripts(
+    paths_files=paths_files_j, reference_mag_lo=12.5, reference_mag_hi=15.5, **kwargs
+)
+write_scripts(
+    paths_files=paths_files_h, reference_mag_lo=12.0, reference_mag_hi=15.0, **kwargs
+)
+write_scripts(
+    paths_files=paths_files_ks, reference_mag_lo=11.5, reference_mag_hi=14.5, **kwargs
+)
