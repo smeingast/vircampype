ac54a785e2518768b5c0c1184d0b16221c389ad8
stefan <stefan.meingast@gmail.com>
2024-01-30 14:18:13 +0100

    Added __repr__ and __str__ for SourceMasks

--- vircampype/miscellaneous/sourcemasks.py
+++ vircampype/miscellaneous/sourcemasks.py
@@ -31,6 +31,12 @@ class SourceMasks:
         self.regions = regions
         self.name = name
 
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
     @property
     def ra(self):
         return Quantity([r.center.icrs.ra for r in self.regions])
