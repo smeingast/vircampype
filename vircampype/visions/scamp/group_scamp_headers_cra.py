4d45b1713a1994d1cc4eb029c66345bc7c031bf7
stefan <stefan.meingast@gmail.com>
2024-01-31 14:38:15 +0100

    Added cloud path from env variable; searching only for script in directories starting with "all"

--- vircampype/visions/scamp/group_scamp_headers_cra.py
+++ vircampype/visions/scamp/group_scamp_headers_cra.py
@@ -3,10 +3,11 @@ import glob
 from visions.scamp.group_scamp_headers import group_scamp_headers
 
 # Find scripts for VISIONS
-path_pipe = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/"
-path_base = f"{path_pipe}scripts/CrA/"
+path_pipe = f"{os.environ['CLOUD_HOME']}/Projects/VISIONS/Pipeline/"
 path_gaia_raw = f"{path_pipe}scamp/CrA/gaia_edr3_raw.fits"
-paths_scripts = sorted(glob.glob(path_base + "/**/*yml"))
+path_base = f"{path_pipe}scripts/CrA/"
+paths_scripts = sorted(glob.glob(path_base + "all*/**/*yml", recursive=True))
+
 # Remove symbolic links
 paths_scripts = [ps for ps in paths_scripts if not os.path.islink(ps)]
 
