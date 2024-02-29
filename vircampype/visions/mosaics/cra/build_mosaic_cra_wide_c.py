b6c8ae12d7698df2b904a3a3673ba4c0fbda50ee
stefan <stefan.meingast@gmail.com>
2024-02-29 14:32:30 +0100

    Updated paths

--- vircampype/visions/mosaics/cra/build_mosaic_cra_wide_c.py
+++ vircampype/visions/mosaics/cra/build_mosaic_cra_wide_c.py
@@ -3,9 +3,9 @@ from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "CrA_mosaic_H_C"
-path_visions = "/Users/stefan/Dropbox/Projects/VISIONS/"
+path_visions = "/Users/stefan/iCloud/Projects/VISIONS/"
 path_scripts = f"{path_visions}Pipeline/scripts/CrA/wide_C/"
-path_master_astro_photo = f"{path_visions}Pipeline/other/master-astro-photo/CrA_wide/"
+path_master_astro_photo = f"{path_visions}Pipeline/master-astro-photo/CrA_wide/"
 path_data = f"/Volumes/Data/Mosaics/CrA/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/CrA/vircampype/"
 projection = "Corona_Australis_wide"
@@ -27,5 +27,5 @@ build_mosaic(
     projection=projection,
     photerr_internal=photerr_internal,
     build_public_catalog=True,
-    source_classification=False,
+    source_classification=True,
 )
