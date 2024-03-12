9cd6d53e5b1dc7a0187fb67dcad2c4738c126ea6
stefan <stefan.meingast@gmail.com>
2024-03-12 11:25:14 +0100

    Adapted to new setup keywords

--- vircampype/visions/mosaics/cra/build_mosaic_cra_wide_c.py
+++ vircampype/visions/mosaics/cra/build_mosaic_cra_wide_c.py
@@ -6,11 +6,14 @@ name = "CrA_mosaic_H_C"
 path_visions = "/Users/stefan/iCloud/Projects/VISIONS/"
 path_scripts = f"{path_visions}Pipeline/scripts/CrA/wide_C/"
 path_master_astro_photo = f"{path_visions}Pipeline/master-astro-photo/CrA_wide/"
+path_master_astro = f"{path_master_astro_photo}MASTER-ASTROMETRY.fits.tab"
+path_master_photo = f"{path_master_astro_photo}MASTER-PHOTOMETRY.fits.tab"
 path_data = f"/Volumes/Data/Mosaics/CrA/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/CrA/vircampype/"
 projection = "Corona_Australis_wide"
-reference_mag_lim = (12.0, 15.0)  # H wide
-photerr_internal = 0.005
+reference_mag_lo = 12.0
+reference_mag_hi = 15.0
+photometric_error_floor = 0.005
 n_jobs = 10
 
 # Get script paths
@@ -22,10 +25,12 @@ build_mosaic(
     path_data=path_data,
     path_pype=path_pype,
     n_jobs=n_jobs,
-    path_master_astro_photo=path_master_astro_photo,
-    reference_mag_lim=reference_mag_lim,
+    path_master_astro=path_master_astro,
+    path_master_photo=path_master_photo,
+    reference_mag_lo=reference_mag_lo,
+    reference_mag_hi=reference_mag_hi,
     projection=projection,
-    photerr_internal=photerr_internal,
+    photometric_error_floor=photometric_error_floor,
     build_public_catalog=True,
     source_classification=True,
 )
