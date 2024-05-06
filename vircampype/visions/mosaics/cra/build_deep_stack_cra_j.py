e84443b25d3faaeb8b784f79472dfe161f6477d8
stefan <stefan.meingast@gmail.com>
2024-05-06 17:58:18 +0200

    Updated with new kwargs

--- vircampype/visions/mosaics/cra/build_deep_stack_cra_j.py
+++ vircampype/visions/mosaics/cra/build_deep_stack_cra_j.py
@@ -3,15 +3,21 @@ from vircampype.visions.mosaics.build_mosaic import build_mosaic
 
 # Setup for pipeline
 name = "CrA_deep_J"
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/deep/"
-path_master_astro_photo = (
-    "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/other/master-astro-photo/CrA_deep/"
-)
+dir_visions = "/Users/stefan/iCloud/Projects/VISIONS/"
+dir_scripts = f"{dir_visions}Pipeline/scripts/CrA/deep/"
+dir_master_astro_photo = f"{dir_visions}/Pipeline/master-astro-photo/CrA_deep/"
+path_master_astro = f"{dir_master_astro_photo}MASTER-ASTROMETRY.fits.tab"
+path_master_photo = f"{dir_master_astro_photo}MASTER-PHOTOMETRY.fits.tab"
 path_data = f"/Volumes/Data/Mosaics/CrA/{name}/"
 path_pype = f"/Volumes/Data/Mosaics/CrA/vircampype/"
+projection = "Corona_Australis_deep"
+reference_mag_lo = 12.5
+reference_mag_hi = 15.5
+photometric_error_floor = 0.005
+n_jobs = 10
 
-# Get script paths
-paths_scripts = sorted(glob.glob(f"{path_scripts}*_J_*.yml"))
+# Get all script paths
+paths_scripts = sorted(glob.glob(f"{dir_scripts}*_J_*.yml"))
 
 # Build mosaic
 build_mosaic(
@@ -19,10 +25,13 @@ build_mosaic(
     paths_scripts=paths_scripts,
     path_data=path_data,
     path_pype=path_pype,
-    path_master_astro_photo=path_master_astro_photo,
-    n_jobs=10,
-    reference_mag_lim=(12.5, 15.5),
-    projection="Corona_Australis_deep",
-    photerr_internal=0.005,
+    path_master_astro=path_master_astro,
+    path_master_photo=path_master_photo,
+    n_jobs=n_jobs,
+    reference_mag_lo=reference_mag_lo,
+    reference_mag_hi=reference_mag_hi,
+    projection=projection,
+    photometric_error_floor=photometric_error_floor,
     build_public_catalog=True,
+    source_classification=True,
 )
