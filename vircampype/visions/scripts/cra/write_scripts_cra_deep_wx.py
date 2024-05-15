de82b160456064546c1024ef576d40a67ce21656
stefan <stefan.meingast@gmail.com>
2024-05-15 14:45:42 +0200

    Updated with new variables

--- vircampype/visions/scripts/cra/write_scripts_cra_deep_wx.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_deep_wx.py
@@ -1,17 +1,20 @@
+import os
 from glob import glob
 from vircampype.visions.scripts.write_scripts import write_scripts
 
 # Define paths
-path_scripts = "/Users/stefan/Dropbox/Projects/VISIONS/Pipeline/scripts/CrA/deep_wx/"
+cloud_home = os.environ["CLOUD_HOME"]
+path_scripts = f"{cloud_home}/Projects/VISIONS/Pipeline/scripts/CrA/deep_wx/"
 path_data = "/Volumes/Data/VISIONS/198C-2009M_CrA/CrA_deep_wx_1/"
 path_pype = "/Volumes/Data/VISIONS/198C-2009M_CrA/vircampype/"
 
 # Search for files
 paths_files = sorted(glob(path_data + "*.fits"))
 
-# Common kwargs
-kwargs = dict(
-    additional_source_masks="Corona_Australis_deep",
+# Write scripts
+write_scripts(
+    paths_files=paths_files,
+    additional_source_masks="Corona_Australis_wide",
     archive=False,
     build_phase3=False,
     build_public_catalog=True,
@@ -23,8 +26,8 @@ kwargs = dict(
     projection="Corona_Australis_deep",
     n_jobs=18,
     external_headers=True,
+    reference_mag_lo=12.0,
+    reference_mag_hi=15.0,
     name_suffix=None,
+    source_classification=True,
 )
-
-# Write scripts
-write_scripts(paths_files=paths_files, reference_mag_lim=(12.0, 15.0), **kwargs)
