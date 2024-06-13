1bfedaeb51538327b341971d1cd878774004848e
stefan <stefan.meingast@gmail.com>
2024-06-13 13:06:32 +0200

    Fixed pipeline path

--- vircampype/visions/scripts/cra/write_scripts_cra_wide_f.py
+++ vircampype/visions/scripts/cra/write_scripts_cra_wide_f.py
@@ -4,6 +4,7 @@ from vircampype.visions.locations import path_data_pleiades, path_visions_proj
 
 # Define paths
 path_data = f"{path_data_pleiades}VISIONS/198C-2009H/data_wide/"
+path_pype = f"{path_data_pleiades}VISIONS/198C-2009H/vircampype/"
 path_scripts = f"{path_visions_proj}Pipeline/scripts/CrA/wide_F/"
 
 # Search for files
@@ -11,7 +12,7 @@ paths_files_1 = sorted(glob(path_data + "CrA*/B/*.fits"))
 
 # Common kwargs
 kwargs = dict(
-    path_pype="/Volumes/Data/VISIONS/198C-2009H/vircampype/",
+    path_pype=path_pype,
     path_scripts=path_scripts,
     archive=False,
     build_phase3=False,
